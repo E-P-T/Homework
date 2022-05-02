c643d530e4c2bb95aa3e66c2d187d1ed09a1eb92
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-05-02 11:31:22 +0400

    Updated Task 6.7, Implemented Task 6.8

--- UladzimirFiodarau/Task 6.7.py
+++ UladzimirFiodarau/Task 6.7.py
@@ -1,4 +1,4 @@
-# Task 4.7
+# Task 6.7
 # Implement a class Money to represent value and currency. You need to implement methods to use all basic arithmetics
 # expressions (comparison, division, multiplication, addition and subtraction). Tip: use class attribute exchange rate
 # which is dictionary and stores information about exchange rates to your default currency:
@@ -22,9 +22,6 @@ class Money(object):
     def __str__(self):
         return f'{self._value} {self._currency}'
 
-    def __repr__(self):
-        return Money({self._value}, {self._currency})
-
     def get_value(self):
         return self._value
 
@@ -32,73 +29,73 @@ class Money(object):
         return self._currency
 
     def convert(self, other):
-        return round(self._value * self.exchange_rate[other.get_curr()] / self.exchange_rate[self.get_curr()], 4)
+        return round(self.get_value() * self.exchange_rate[other.get_curr()] / self.exchange_rate[self.get_curr()], 4)
 
     def __add__(self, other):
         if isinstance(other, Money):
-            return Money(round(self._value + other.convert(self), 4), self._currency)
+            return Money(round(self.get_value() + other.convert(self), 4), self.get_curr())
         elif isinstance(other, int) or isinstance(other, float):
-            return Money(round(self._value + other, 4), self._currency)
+            return Money(round(self.get_value() + other, 4), self.get_curr())
 
     def __sub__(self, other):
         if isinstance(other, Money):
-            return Money(round(self._value - other.convert(self), 4), self._currency)
+            return Money(round(self.get_value() - other.convert(self), 4), self.get_curr())
         elif isinstance(other, int) or isinstance(other, float):
-            return Money(round(self._value - other, 4), self._currency)
+            return Money(round(self.get_value() - other, 4), self.get_curr())
 
     def __mul__(self, other):
         if isinstance(other, Money):
-            return Money(round(self._value * other.convert(self), 4), self._currency)
+            return Money(round(self.get_value() * other.convert(self), 4), self.get_curr())
         elif isinstance(other, int) or isinstance(other, float):
-            return Money(round(self._value * other, 4), self._currency)
+            return Money(round(self.get_value() * other, 4), self.get_curr())
 
     def __truediv__(self, other):
         if isinstance(other, Money):
-            return Money(round(self._value / other.convert(self), 4), self._currency)
+            return Money(round(self.get_value() / other.convert(self), 4), self.get_curr())
         elif isinstance(other, int) or isinstance(other, float):
-            return Money(round(self._value / other, 4), self._currency)
+            return Money(round(self.get_value() / other, 4), self.get_curr())
 
     def __eq__(self, other):
         if isinstance(other, Money):
-            return round(self._value, 4) == round(other.convert(self), 4)
+            return round(self.get_value(), 4) == round(other.convert(self), 4)
         elif isinstance(other, int) or isinstance(other, float):
-            return round(self._value, 4) == round(other, 4)
+            return round(self.get_value(), 4) == round(other, 4)
 
     def __eq__(self, other):
         if isinstance(other, Money):
-            return round(self._value, 4) == round(other.convert(self), 4)
+            return round(self.get_value(), 4) == round(other.convert(self), 4)
         elif isinstance(other, int) or isinstance(other, float):
-            return round(self._value, 4) == round(other, 4)
+            return round(self.get_value(), 4) == round(other, 4)
 
     def __ne__(self, other):
         if isinstance(other, Money):
-            return round(self._value, 4) != round(other.convert(self), 4)
+            return round(self.get_value(), 4) != round(other.convert(self), 4)
         elif isinstance(other, int) or isinstance(other, float):
-            return round(self._value, 4) != round(other, 4)
+            return round(self.get_value(), 4) != round(other, 4)
 
     def __gt__(self, other):
         if isinstance(other, Money):
-            return round(self._value, 4) > round(other.convert(self), 4)
+            return round(self.get_value(), 4) > round(other.convert(self), 4)
         elif isinstance(other, int) or isinstance(other, float):
-            return round(self._value, 4) > round(other, 4)
+            return round(self.get_value(), 4) > round(other, 4)
 
     def __ge__(self, other):
         if isinstance(other, Money):
-            return round(self._value, 4) >= round(other.convert(self), 4)
+            return round(self.get_value(), 4) >= round(other.convert(self), 4)
         elif isinstance(other, int) or isinstance(other, float):
-            return round(self._value, 4) >= round(other, 4)
+            return round(self.get_value(), 4) >= round(other, 4)
 
     def __lt__(self, other):
         if isinstance(other, Money):
-            return round(self._value, 4) < round(other.convert(self), 4)
+            return round(self.get_value(), 4) < round(other.convert(self), 4)
         elif isinstance(other, int) or isinstance(other, float):
-            return round(self._value, 4) < round(other, 4)
+            return round(self.get_value(), 4) < round(other, 4)
 
     def __le__(self, other):
         if isinstance(other, Money):
-            return round(self._value, 4) <= round(other.convert(self), 4)
+            return round(self.get_value(), 4) <= round(other.convert(self), 4)
         elif isinstance(other, int) or isinstance(other, float):
-            return round(self._value, 4) <= round(other, 4)
+            return round(self.get_value(), 4) <= round(other, 4)
 
 
     __radd__ = __add__
@@ -107,29 +104,29 @@ class Money(object):
     __rtruediv__ = __truediv__
 
 
-# x = Money(10, "BYN")
-# y = Money(11, "USD")
-# z = Money(12.34, "EUR")
-# print(x)
-# # 10 BYN
-#
-#
-# print(x != y)
-# # True
-# p = Money(10, "BYN")
-# print(x == p)
-# # True
-#
-# print(y > z)
-# # False
-#
-# print(x < y)
-# # True
-#
-# print(z + 3.11 * x + y * 0.8) # result in “EUR”
-# # 31.8594 EUR
-#
-# L = [x, y, z]
-# s = sum(L)
-# print(s)
-# # 73.6102 BYN
+x = Money(10, "BYN")
+y = Money(11, "USD")
+z = Money(12.34, "EUR")
+print(x)
+# 10 BYN
+
+
+print(x != y)
+# True
+p = Money(10, "BYN")
+print(x == p)
+# True
+
+print(y > z)
+# False
+
+print(x < y)
+# True
+
+print(z + 3.11 * x + y * 0.8) # result in “EUR”
+# 31.8594 EUR
+
+L = [x, y, z]
+s = sum(L)
+print(s)
+# 73.6102 BYN
