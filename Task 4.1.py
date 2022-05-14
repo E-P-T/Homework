f848f77c864dcb116c98e693b6596362db2bcfaf
fcumay <voyshnism@mail.ru>
2022-05-14 23:09:50 +0300

    Update Task 4.1 rmt

--- Task 4.1.py
+++ Task 4.1.py
@@ -1,5 +1,5 @@
 def replace(my_string):
     return my_string.replace('"','~').replace("'",'"').replace('~',"'")
 if __name__ == '__main__':
-    my_string=input('Input your string: ')
+    my_string=input('Input your string:')
     print(replace(my_string))
