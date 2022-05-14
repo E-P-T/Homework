4b1526becff00f67c71fac74bae514e1ceaae8ce
fcumay <voyshnism@mail.ru>
2022-05-14 23:12:24 +0300

    Moved Tasks to MayaVoyshnis

--- MayaVoyshnis/Task 4.1.py
+++ MayaVoyshnis/Task 4.1.py
@@ -1,5 +1,7 @@
 def replace(my_string):
-    return my_string.replace('"','~').replace("'",'"').replace('~',"'")
+    return my_string.replace('"', '~').replace("'", '"').replace('~', "'")
+
+
 if __name__ == '__main__':
-    my_string=input('Input your string:')
+    my_string = input('Input your string:')
     print(replace(my_string))
