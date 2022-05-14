ef15a8e9090cd4c9501f4c8932163ad2a568e4b8
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-05-14 19:46:39 +0400

    Updated test subject, implemented Tasks 8.1 - test_goldbach.py and task 8.2 - test_goldbach_ete.py

--- UladzimirFiodarau/goldbach.py
+++ UladzimirFiodarau/goldbach.py
@@ -1,19 +1,14 @@
-# Task 8.1
-# Using mock write unit test for task 7.6.
 # Create console program for proving Goldbach's conjecture. Program accepts number for input and print result.
 # For pressing 'q' program succesfully close. Use function from Task 5.5 for validating input,
 # handle all exceptions and print user friendly output.
 # The conjecture states that every even number starting with 4 can be expressed as the sum of two prime numbers.
-
-
-
 class CustomException(BaseException):
 
-    def __init__(self, error_message=''):
+    def __init__(self, error_message: str = ''):
         self.message = error_message
 
     def __str__(self):
-        return self.message
+        return str(self.message)
 
 
 class TypeException(CustomException):
@@ -28,10 +23,6 @@ class ValueException(CustomException):
     pass
 
 
-class StringException(CustomException):
-    pass
-
-
 def is_even(num: int = None) -> bool:
     if num is None:
         raise EmptyException("Function requires directly one integer argument")
@@ -40,12 +31,16 @@ def is_even(num: int = None) -> bool:
 
     return num % 2 == 0
 
-def get_primes(num: int) -> list:
+
+def get_primes(num: int = None) -> list:
     """
-    The function takes an integer and returns a list of primes in range [2, integer + 1)
+    The function takes a positive integer and returns a list of primes in range [2, integer + 1)
     :param num: input integer
     :return: list of corresponding primes
     """
+    if num is None:
+        raise EmptyException("Function requires directly one integer argument")
+    assert isinstance(num, int) and num >= 0, "Wrong argument type, must be a positive integer"
     primes = list(range(2, num + 1))
     for number in primes:
         i = 2
@@ -55,19 +50,17 @@ def get_primes(num: int) -> list:
             i += 1
     return primes
 
-def find_goldbach(num: int) -> tuple:
+
+def find_goldbach(num: int = None) -> tuple:
     """
-    the function takes an integer and returns
-    :param num:
+    the function takes an integer and if it is a positive integer greater or equal to 4 then
+    returns a tuple that consists of the integer and a list of two prime numbers which sum forms the integer
+    :param num: input integer
     :return:
     """
     if is_even(num):
         if num < 4:
             raise ValueException("Input integer doesn't satisfy conjecture assumings")
-        elif num == 4:
-            return 4, [2, 2]
-        elif num == 6:
-            return 6, [3, 3]
         else:
             primes = get_primes(num)
             combinations = [[x, num - x] for x in primes if num - x in primes]
@@ -75,32 +68,33 @@ def find_goldbach(num: int) -> tuple:
     else:
         raise ValueException("Input integer doesn't satisfy conjecture assumings")
 
+
 def get_input():
-    return input("Input a positive even integer, or 'q' to exit script: ")
+    return input("Input a positive even integer to prove Goldbach's conjecture for it, or 'q' to exit script: ")
+
 
 def main():
-    command = get_input()
-    if command in ('q', 'Q'):
-        print('Thanks for using script and have a good day')
-    else:
-        try:
-            num = int(command)
-        except:
-            num = command
+    while True:
+        command = get_input()
+        if command in ('q', 'Q'):
+            print('Thanks for using script and have a good day')
+            break
+        else:
+            try:
+                num = int(command)
+            except ValueError:
+                num = command if command else None
 
-        try:
-            print(find_goldbach(num))
-        except ValueException as message:
-            print(f'Incorrect input: {message}')
-        except EmptyException as message:
-            print(f'Error ocured during function runtime: {message}')
-        except TypeException as message:
-            print(f'Error ocured during function runtime: {message}')
-        except Exception as message:
-            print(f'Something got wrong: {message}')
-        finally:
-            main()
-
+            try:
+                print(find_goldbach(num))
+            except ValueException as message:
+                print(f'Incorrect input: {message}')
+            except EmptyException as message:
+                print(f'Incorrect input: {message}')
+            except TypeException as message:
+                print(f'Incorrect input: {message}')
+            except Exception as message:
+                print(f'Something got wrong: {message}')
 
 
 if __name__ == '__main__':
