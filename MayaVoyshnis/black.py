04d8d7433972cc8fc48a53bd30eebb2f70b6fa53
fcumay <voyshnism@mail.ru>
2022-05-20 01:44:19 +0300

    Delete useless

---
+++ MayaVoyshnis/black.py
@@ -0,0 +1,16 @@
+import time
+
+def timer(f):
+    def wrapper(sec):
+        t0=time.time()
+        res=f(sec)
+        t1=time.time()
+        print(f'Function execution time: {t1-t0:.2f}')
+        return res
+    return wrapper
+
+@timer
+def sleeper(sec):
+    time.sleep(sec)
+
+sleeper(5)
