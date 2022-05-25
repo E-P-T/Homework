a1bbdce1033453970a31a6dd45733140fb97abb5
J D <ab@c.ru>
2022-05-26 01:55:34 +0300

    Create an get_divisors function

--- AndreyOzerets/Task3-2.py
+++ AndreyOzerets/Task3-2.py
@@ -1,5 +1,7 @@
 # Task 2.3-2
 
+from typing import List
+
 
 def get_number(greeting: str) -> int:
     '''Get number from user.'''
@@ -10,3 +12,9 @@ def get_number(greeting: str) -> int:
         except ValueError:
             print("You entered not a number!!! Try it again...")
     return number
+
+
+def get_divisors(number: int) -> List[int]:
+    '''Get the divisors of a number.'''
+    divisors = [i for i in range(1, number+1) if number % i == 0]
+    return divisors
