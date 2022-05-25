d89fb510049c5bcbfd1913c0dd2f304ea5acd7a8
J D <ab@c.ru>
2022-05-26 01:54:16 +0300

    Create an get_number function

--- AndreyOzerets/Task3-2.py
+++ AndreyOzerets/Task3-2.py
@@ -1,1 +1,12 @@
 # Task 2.3-2
+
+
+def get_number(greeting: str) -> int:
+    '''Get number from user.'''
+    while True:
+        try:
+            number = int(input(greeting))
+            break
+        except ValueError:
+            print("You entered not a number!!! Try it again...")
+    return number
