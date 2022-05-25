a172fcbd657d79882d37c9945a9c0da1b86aff9d
J D <ab@c.ru>
2022-05-26 00:57:37 +0300

    Get the number of characters in a string

--- AndreyOzerets/Task1.py
+++ AndreyOzerets/Task1.py
@@ -1,8 +1,7 @@
 # Task 2.1
 
 def get_str(greeting: str) -> str:
-    '''Return the entered string
-    '''
+    '''Return the entered string.'''
     seq = input(greeting)
     return seq
 
@@ -10,3 +9,9 @@ def get_str(greeting: str) -> str:
 def len_str(string: str) -> int:
     '''Count the number of characters.'''
     return sum(1 for _ in string)
+
+
+if __name__ == '__main__':
+    print()
+    print('{:*^30}'.format('The task 2.1'), end='\n\n')
+    print(f'The number of characters is {len_str(get_str("Enter string: "))}')
