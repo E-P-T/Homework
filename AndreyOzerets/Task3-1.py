9c7b0ae41261d98fb9f5d8eec2ef435eb4a1e5d5
J D <ab@c.ru>
2022-05-26 01:37:09 +0300

    Create an break_into_words function

--- AndreyOzerets/Task3-1.py
+++ AndreyOzerets/Task3-1.py
@@ -1,6 +1,15 @@
 # Task 2.3-1
 
+from typing import List
+
+
 def get_words(greeting: str) -> str:
     '''Return the entered string.'''
     seq = input(greeting)
     return seq
+
+
+def break_into_words(raw_str: str, sep: str = ',') -> List[str]:
+    '''Split string at separator.'''
+    words = raw_str.split(sep)
+    return words
