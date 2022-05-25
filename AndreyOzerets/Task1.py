e880db7ea1d5f011a2b04ee9f4dad8558b64d5df
J D <ab@c.ru>
2022-05-26 00:46:50 +0300

    Create an len_str function

--- AndreyOzerets/Task1.py
+++ AndreyOzerets/Task1.py
@@ -5,3 +5,8 @@ def get_str(greeting: str) -> str:
     '''
     seq = input(greeting)
     return seq
+
+
+def len_str(string: str) -> int:
+    '''Count the number of characters.'''
+    return sum(1 for _ in string)
