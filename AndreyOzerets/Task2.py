6848750327987241cf0a01b161a9e748c93737fb
J D <ab@c.ru>
2022-05-26 01:21:01 +0300

    Create an count function

--- AndreyOzerets/Task2.py
+++ AndreyOzerets/Task2.py
@@ -1,5 +1,8 @@
 # Task 2.2
 
+from collections import Counter
+from typing import Dict
+
 
 def get_str(greeting: str) -> str:
     '''Return the entered string.'''
@@ -10,3 +13,8 @@ def get_str(greeting: str) -> str:
 def get_lower_str(string: str) -> str:
     '''Return lowercase string.'''
     return string.lower()
+
+
+def count(string: str) -> Dict[str, int]:
+    '''Return a dictionary with the number of characters in a string.'''
+    return dict(Counter(string))
