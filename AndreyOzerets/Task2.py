a3d3482b25b41dd37cf1fb326e5592c305eaa6f8
J D <ab@c.ru>
2022-05-26 01:17:47 +0300

    Create an get_lower_str function

--- AndreyOzerets/Task2.py
+++ AndreyOzerets/Task2.py
@@ -5,3 +5,8 @@ def get_str(greeting: str) -> str:
     '''Return the entered string.'''
     seq = input(greeting)
     return seq
+
+
+def get_lower_str(string: str) -> str:
+    '''Return lowercase string.'''
+    return string.lower()
