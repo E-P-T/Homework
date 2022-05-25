b6293862a96805730c507c35169e44140bd18742
J D <ab@c.ru>
2022-05-26 01:44:40 +0300

    Create an strip_chars function

--- AndreyOzerets/Task3-1.py
+++ AndreyOzerets/Task3-1.py
@@ -13,3 +13,12 @@ def break_into_words(raw_str: str, sep: str = ',') -> List[str]:
     '''Split string at separator.'''
     words = raw_str.split(sep)
     return words
+
+
+def strip_chars(words: List[str], chars: str = ' ') -> List[str]:
+    '''Return strings with the leading and trailing characters removed.'''
+    clean_words: List[str] = []
+    clean_words_append = clean_words.append
+    for i in words:
+        clean_words_append(i.strip(chars))
+    return clean_words
