23a383a6c8ead15c52e92969a74ee66d86f13a4a
J D <ab@c.ru>
2022-05-26 01:25:00 +0300

    Get the number of characters in a string

--- AndreyOzerets/Task2.py
+++ AndreyOzerets/Task2.py
@@ -18,3 +18,10 @@ def get_lower_str(string: str) -> str:
 def count(string: str) -> Dict[str, int]:
     '''Return a dictionary with the number of characters in a string.'''
     return dict(Counter(string))
+
+
+if __name__ == '__main__':
+    print()
+    print('{:*^30}'.format('The task 2.2'), end='\n\n')
+    str_ = get_str('Enter string: ')
+    print(f'Result: {count(get_lower_str(str_))}')
