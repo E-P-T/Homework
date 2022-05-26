ee5399e63a89984d0aa24c1ef6f492b8a546bc19
J D <ab@c.ru>
2022-05-26 15:31:53 +0300

    Replace all \" symbols with \' and vise versa

--- AndreyOzerets/Task1.py
+++ AndreyOzerets/Task1.py
@@ -19,3 +19,15 @@ def get_char(str_: str, ch1: str, ch2: str) -> Iterable[str]:
 def replaces_symbols(gen):
     '''Return a new string from the given characters'''
     return ''.join(i for i in gen)
+
+
+if __name__ == '__main__':
+    s = '''Implement a function which receives a string " and ' replaces
+all `"` symbols with `'` and vise versa.'''
+
+    print()
+    print('{:*^30}'.format('Task 4.1'), end='\n\n')
+    print(f'New string:')
+    g = get_char(s, "'", '"')
+    print(replaces_symbols(g))
+    print()
