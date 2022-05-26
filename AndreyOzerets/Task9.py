470b85cc314d4d8f6a2aeecadb4c01160e0175a6
J D <ab@c.ru>
2022-05-27 00:47:00 +0300

    Create a test_1_3 function

--- AndreyOzerets/Task9.py
+++ AndreyOzerets/Task9.py
@@ -13,3 +13,15 @@ def test_1_2(*args):
     '''Return characters that occur in at least one string'''
 
     return reduce(lambda a, b: a | b, map(set, args))
+
+
+def test_1_3(*args):
+    '''Return characters that occur in at least two strings'''
+
+    res = set()
+
+    for i in args:
+        for j in args:
+            if i != j:
+                res |= set(i) & set(j)
+    return res
