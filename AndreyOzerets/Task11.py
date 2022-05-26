ff008c167f7e96136efd0a68d91762773703dd36
J D <ab@c.ru>
2022-05-27 01:12:01 +0300

    Combine a variable number of dictionaries into one

--- AndreyOzerets/Task11.py
+++ AndreyOzerets/Task11.py
@@ -15,3 +15,16 @@ def combine_dicts(*args):
             else:
                 result[j] += i[j]
     return result
+
+
+if __name__ == '__main__':
+    print()
+    print('{:*^30}'.format('Task 4.11'), end='\n\n')
+
+    a = {'a': 100, 'b': 200}
+    b = {'a': 200, 'c': 300}
+    c = {'a': 300, 'd': 100}
+    d = {'a': 100, 'f': 300}
+
+    print(f'Result 1: {combine_dicts(a, b, c, d)}', end='\n\n')
+    print(f'Result 2: {combine_dicts(a, b, d)}', end='\n\n')
