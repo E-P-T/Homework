b70f549a058ebffa1ffa07d1c4d53d16f0287f10
J D <ab@c.ru>
2022-05-27 01:04:36 +0300

    Get the result of the function generate_squares for the number 5

--- AndreyOzerets/Task10.py
+++ AndreyOzerets/Task10.py
@@ -12,3 +12,9 @@ def generate_squares(n: int) -> Dict[int, int]:
     for i in range(1, n + 1):
         result[i] = i ** 2
     return result
+
+
+if __name__ == '__main__':
+    print()
+    print('{:*^30}'.format('Task 4.10'), end='\n\n')
+    print(f'Result: {generate_squares(5)}')
