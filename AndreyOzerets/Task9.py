ab7d4c7931b5e4923a3b242122fc8c0692d41b5d
J D <ab@c.ru>
2022-05-27 00:44:29 +0300

    Create a test_1_2 function

--- AndreyOzerets/Task9.py
+++ AndreyOzerets/Task9.py
@@ -7,3 +7,9 @@ def test_1_1(*args):
     '''Return characters that occur in all strings'''
 
     return reduce(lambda a, b: a & b, map(set, args))
+
+
+def test_1_2(*args):
+    '''Return characters that occur in at least one string'''
+
+    return reduce(lambda a, b: a | b, map(set, args))
