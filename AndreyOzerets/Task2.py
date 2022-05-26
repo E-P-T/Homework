61c843c34ebbf0ea68be19f96bda68bf228a155e
J D <ab@c.ru>
2022-05-27 02:28:32 +0300

    Create words function

--- AndreyOzerets/Task2.py
+++ AndreyOzerets/Task2.py
@@ -1,1 +1,17 @@
 # Task 5.2
+
+from typing import List, Optional
+
+
+def words(string_: str,
+          punct: str,
+          empty_line: str = '\n',
+          **kwargs) -> Optional[List[str]]:
+    '''Return a word list of a string'''
+
+    if not string_ == empty_line:
+        raw_words = string_.lower().split(**kwargs)
+        words = [i.strip(punct) for i in raw_words]
+        return words
+    else:
+        return None
