d861bee9f986a7ce2c3ac93463da4e49d947906f
J D <ab@c.ru>
2022-05-26 15:49:22 +0300

    Create a new_split function

--- AndreyOzerets/Task3.py
+++ AndreyOzerets/Task3.py
@@ -1,2 +1,23 @@
 # The Task 4.3
 
+from typing import List
+
+
+def new_split(str_: str, sep: str = ' ', maxsplit: int = -1) -> List[str]:
+    '''Return the given number of words from a string.'''
+
+    out: List[str] = []
+    out_append = out.append
+
+    start = 0
+
+    while maxsplit:
+        stop = str_.find(sep, start)
+        if stop == -1:
+            out_append(str_[start:])
+            break
+        else:
+            out_append(str_[start:stop])
+            start = stop + 1
+            maxsplit -= 1
+    return out
