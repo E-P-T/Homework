aa093ede3aff8b69c407a25f54691077b3c2fecc
J D <ab@c.ru>
2022-05-26 13:20:23 +0300

    Create an with_join function

--- AndreyOzerets/Task6.py
+++ AndreyOzerets/Task6.py
@@ -1,1 +1,13 @@
 # Task 2.6
+
+from typing import Tuple
+
+
+def with_join(numbers: Tuple[int]) -> int:
+    '''Convert a tuple of positive integers to a number
+
+    Implemented via join and generator comprehension.
+    '''
+    gen_numbers = (str(i) for i in numbers)
+    str_number = int(''.join(gen_numbers))
+    return str_number
