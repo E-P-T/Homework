2622bc83796f56650b1181c7455b9212d578c78a
J D <ab@c.ru>
2022-05-27 02:19:12 +0300

    Sort by name and write to file

--- AndreyOzerets/Task1.py
+++ AndreyOzerets/Task1.py
@@ -1,8 +1,9 @@
 # Task 5.1
 
+from heapq import merge
 from itertools import islice
 from typing import Iterable, List
-from tempfile import _TemporaryFileWrapper
+from tempfile import _TemporaryFileWrapper, TemporaryFile
 
 
 def line_from_file(file: str, end: str = '\n', **kwargs) -> Iterable[str]:
@@ -42,3 +43,26 @@ def close_tmp_files(tmp_files: List[_TemporaryFileWrapper]) -> None:
 
     for f in tmp_files:
         f.close()
+
+
+if __name__ == '__main__':
+    sorted_tmp_files: List[_TemporaryFileWrapper] = []
+    sorted_tmp_files_append = sorted_tmp_files.append
+    print()
+    print('{:*^30}'.format('The task 5.1'), end='\n\n')
+
+    try:
+        for chunk in get_chunk(line_from_file('data/unsorted_names.txt')):
+            chunk.sort()
+            tmp_file = TemporaryFile('w+')
+            tmp_file_with_data = write_to_tmp_file(tmp_file, chunk)
+            sorted_tmp_files_append(tmp_file_with_data)
+    except FileNotFoundError as er:
+        print(er, end='\n\n')
+    else:
+        with open('data/sorted_names.txt', 'w') as output_file:
+            output_file.writelines(merge(*sorted_tmp_files))
+
+        print(f'Names sorted and wrote to file', end='\n\n')
+    finally:
+        close_tmp_files(sorted_tmp_files)
