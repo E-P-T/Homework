f84c4f09a1a92d079bf6b153752434e24fd3911c
J D <ab@c.ru>
2022-05-26 16:04:48 +0300

    Create option_1 function

--- AndreyOzerets/Task5.py
+++ AndreyOzerets/Task5.py
@@ -1,1 +1,12 @@
 # The Task 4.5
+
+
+from typing import Tuple
+
+
+def option_1(num: int) -> Tuple[int]:
+    '''Returns a tuple of a given integer's digits
+
+    Option number 1.
+    '''
+    return tuple(int(i) for i in str(num))
