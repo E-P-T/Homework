83ccd35630fe45cfa26ef7a6a92d72b7aab870e3
J D <ab@c.ru>
2022-05-26 16:05:46 +0300

    Create an option_2 function

--- AndreyOzerets/Task5.py
+++ AndreyOzerets/Task5.py
@@ -1,7 +1,7 @@
 # The Task 4.5
 
 
-from typing import Tuple
+from typing import List, Tuple
 
 
 def option_1(num: int) -> Tuple[int]:
@@ -10,3 +10,16 @@ def option_1(num: int) -> Tuple[int]:
     Option number 1.
     '''
     return tuple(int(i) for i in str(num))
+
+
+def option_2(num: int) -> Tuple[int]:
+    '''Returns a tuple of a given integer's digits
+
+    Option number 2.
+    '''
+    digits: List[int] = []
+    digits_append = digits.append
+    while num:
+        digits_append(num % 10)
+        num //= 10
+    return tuple(digits[::-1])
