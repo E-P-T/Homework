a769d3a7bb6d8a1263305034b143badc168486a8
J D <ab@c.ru>
2022-05-26 12:59:01 +0300

    Create an func_comprs function

--- AndreyOzerets/Task5.py
+++ AndreyOzerets/Task5.py
@@ -1,1 +1,11 @@
 # Task 2.5
+
+from typing import Dict, List, Set
+
+
+def func_comprs(data_list: List[Dict[str, str]]) -> Set[str]:
+    '''Return unique dictionary values
+
+    Implemented via set comprehension.
+    '''
+    return {dict_[value] for dict_ in data_list for value in dict_}
