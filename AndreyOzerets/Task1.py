a94dcfa6dc24266d7bd1bd823423b400765c1fcc
J D <ab@c.ru>
2022-05-27 02:02:41 +0300

    Create a line_from_file function

--- AndreyOzerets/Task1.py
+++ AndreyOzerets/Task1.py
@@ -1,1 +1,13 @@
 # Task 5.1
+
+from typing import Iterable
+
+
+def line_from_file(file: str, end: str = '\n', **kwargs) -> Iterable[str]:
+    '''Return a string in one iteration'''
+
+    with open(file, **kwargs) as raw_file:
+        for line in raw_file:
+            if not line.endswith(end):
+                line = line + end
+            yield line
