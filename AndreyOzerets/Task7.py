69c8e77eab418fb9835c01e0859f84fb2c0c6363
J D <ab@c.ru>
2022-05-26 13:33:45 +0300

    Create a data_gen function

--- AndreyOzerets/Task7.py
+++ AndreyOzerets/Task7.py
@@ -1,5 +1,7 @@
 # Task 2.7
 
+from typing import Iterable
+
 
 def indent_length(a: int, b: int, c: int, d: int) -> int:
     '''Return the length of the indent between columns.'''
@@ -9,3 +11,8 @@ def indent_length(a: int, b: int, c: int, d: int) -> int:
     indent_length = len(str(max_ab*max_cd)) + 1
 
     return indent_length
+
+
+def data_gen(a: int, b: int, c: int, d: int) -> Iterable[int]:
+    '''Return the product of two numbers.'''
+    return (i*j for i in range(a, b+1) for j in range(c, d+1))
