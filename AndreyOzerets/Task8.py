41dd3a4649ea7c156283f2b71c8ab76a0c47cfaf
J D <ab@c.ru>
2022-05-27 00:36:35 +0300

    Create a get_pairs function

--- AndreyOzerets/Task8.py
+++ AndreyOzerets/Task8.py
@@ -1,1 +1,19 @@
 # Task 4.8
+
+
+from typing import List, Tuple, Optional
+
+
+def get_pairs(lst: List) -> Optional[List[Tuple]]:
+    '''Return a list of tuples containing pairs of elements'''
+
+    result: Optional[List[Tuple]] = []
+
+    len_ = len(lst)
+
+    if len_ > 1:
+        result = [(lst[i], lst[i+1]) for i in range(len_-1)]
+    else:
+        result = None
+
+    return result
