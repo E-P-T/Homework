4f704cb23eb454ef22b3ff567dcb06d6bbc29ef5
J D <ab@c.ru>
2022-05-26 15:25:09 +0300

    Create an get_char function

--- AndreyOzerets/Task1.py
+++ AndreyOzerets/Task1.py
@@ -1,2 +1,16 @@
 # The Task 4.1
 
+from typing import Iterable
+
+
+def get_char(str_: str, ch1: str, ch2: str) -> Iterable[str]:
+    '''Return each character of a string
+
+    At the same time, it replaces the characters of the string ch1 with ch2.
+    '''
+    for i in str_:
+        if i == ch1:
+            i = ch2
+        elif i == ch2:
+            i = ch1
+        yield i
