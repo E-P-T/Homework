14db5137aba3b9bac9b5b3ff361375bfb4633f25
J D <ab@c.ru>
2022-05-26 23:55:45 +0300

    Create a get_longest_word function

--- AndreyOzerets/Task6.py
+++ AndreyOzerets/Task6.py
@@ -1,1 +1,13 @@
 # The Task 4.6
+
+def get_longest_word(s: str) -> str:
+    '''Return the longest word from a string'''
+
+    words = s.split()
+    best = words[0]
+    len_best = len(best)
+    for v in words:
+        if len_best < len(v):
+            best = v
+            len_best = len(best)
+    return best
