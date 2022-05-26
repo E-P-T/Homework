2e2a53748ce447afaceec2eceaf9a78bfb450209
J D <ab@c.ru>
2022-05-27 00:57:38 +0300

    Create a generate_squares function

--- AndreyOzerets/Task10.py
+++ AndreyOzerets/Task10.py
@@ -1,1 +1,14 @@
 # Task 4.10
+
+from typing import Dict
+
+
+def generate_squares(n: int) -> Dict[int, int]:
+    '''Return dictionary.
+    The key is a number, the value is the square of that number.
+    '''
+
+    result = {}
+    for i in range(1, n + 1):
+        result[i] = i ** 2
+    return result
