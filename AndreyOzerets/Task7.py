407059b3e1e27856d263ccd7c83358cce7b6afdf
J D <ab@c.ru>
2022-05-27 00:24:35 +0300

    Create a foo function

--- AndreyOzerets/Task7.py
+++ AndreyOzerets/Task7.py
@@ -1,1 +1,23 @@
 # Task 4.7
+
+
+from typing import List
+
+
+def foo(data: List[int]) -> List[int]:
+    '''Return a list of products of numbers from the given list.
+
+    The i-st element itself is not taken into account in the calculation.
+    '''
+    result = []
+
+    for i in range(len(data)):
+        accumulation = 1
+        for key, value in enumerate(data):
+            if i == key:
+                continue
+            else:
+                accumulation *= value
+        result.append(accumulation)
+
+    return result
