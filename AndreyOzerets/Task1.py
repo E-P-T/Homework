07e487fc893d1e8c1e33b63bcfc90e3a51ccace0
J D <ab@c.ru>
2022-05-27 02:09:07 +0300

    Create a get_chunk function

--- AndreyOzerets/Task1.py
+++ AndreyOzerets/Task1.py
@@ -1,6 +1,7 @@
 # Task 5.1
 
-from typing import Iterable
+from itertools import islice
+from typing import Iterable, List
 
 
 def line_from_file(file: str, end: str = '\n', **kwargs) -> Iterable[str]:
@@ -11,3 +12,15 @@ def line_from_file(file: str, end: str = '\n', **kwargs) -> Iterable[str]:
             if not line.endswith(end):
                 line = line + end
             yield line
+
+
+def get_chunk(data: Iterable[str],
+              amount_of_elements: int = 10,
+              **kwargs) -> Iterable[List[str]]:
+    '''Return a chunk of string'''
+
+    while True:
+        chunk = list(islice(data, amount_of_elements, **kwargs))
+        if not chunk:
+            break
+        yield chunk
