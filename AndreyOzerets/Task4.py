5e55d0c1ae41be2fd33f3746869023290192845e
J D <ab@c.ru>
2022-05-26 11:22:19 +0300

    Create an data_dict function

--- AndreyOzerets/Task4.py
+++ AndreyOzerets/Task4.py
@@ -1,1 +1,12 @@
 # Task 2.4
+
+def data_dict(source_dict):
+    '''Return a dictionary with data grouped by key type.'''
+    out_dict = {}
+    for i in source_dict:
+        key_type = type(i)
+        if key_type not in out_dict:
+            out_dict[key_type] = {}
+        out_dict[key_type].update({i: source_dict[i]})
+    print(out_dict)
+    return out_dict
