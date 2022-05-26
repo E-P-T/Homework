d77848b10fea3aa8c360f755cc1f523a0f43c3fe
J D <ab@c.ru>
2022-05-27 01:09:42 +0300

    Create a combine_dicts function

--- AndreyOzerets/Task11.py
+++ AndreyOzerets/Task11.py
@@ -1,1 +1,17 @@
 # Task 4.11
+
+
+def combine_dicts(*args):
+    '''Merge a variable number of dictionaries
+
+    In case of identical keys, the values are summed up.
+    '''
+
+    result = {}
+    for i in args:
+        for j in i:
+            if j not in result:
+                result[j] = i[j]
+            else:
+                result[j] += i[j]
+    return result
