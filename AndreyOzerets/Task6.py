f7b1f29117ff187e4344bfac4ab8a5ae99eb90ea
J D <ab@c.ru>
2022-05-26 23:58:39 +0300

    Get the longest word from a string

--- AndreyOzerets/Task6.py
+++ AndreyOzerets/Task6.py
@@ -11,3 +11,14 @@ def get_longest_word(s: str) -> str:
             best = v
             len_best = len(best)
     return best
+
+
+if __name__ == '__main__':
+
+    s = 'Python is simple and effective!'
+    # s = 'Any pythonista like namespaces a lot.'
+
+    print()
+    print('{:*^30}'.format('Task 4.6'), end='\n\n')
+    print(f'A long word: {get_longest_word(s)}')
+    print()
