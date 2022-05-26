44794ebb487bacc11441865e02353ab72bbefd1c
J D <ab@c.ru>
2022-05-26 13:35:32 +0300

    Create a print_tab function

--- AndreyOzerets/Task7.py
+++ AndreyOzerets/Task7.py
@@ -1,6 +1,6 @@
 # Task 2.7
 
-from typing import Iterable
+from typing import Callable, Iterable
 
 
 def indent_length(a: int, b: int, c: int, d: int) -> int:
@@ -16,3 +16,26 @@ def indent_length(a: int, b: int, c: int, d: int) -> int:
 def data_gen(a: int, b: int, c: int, d: int) -> Iterable[int]:
     '''Return the product of two numbers.'''
     return (i*j for i in range(a, b+1) for j in range(c, d+1))
+
+
+def print_tab(a: int,
+              b: int,
+              c: int,
+              d: int,
+              indent: int,
+              gen: Callable[[int, int, int, int], Iterable[int]]) -> None:
+    '''Print data in table view.'''
+
+    data = gen(a, b, c, d)
+
+    print('{:<{y1}}'.format('', y1=indent), end=" ")
+
+    for i in range(c, d+1):
+        print('{:<{y1}}'.format(i, y1=indent), end=" ")
+    print()
+
+    for i in range(a, b+1):
+        print('{:<{y1}}'.format(i, y1=indent), end=" ")
+        for _ in range(c, d+1):
+            print('{:<{y1}}'.format(next(data), y1=indent), end=" ")
+        print()
