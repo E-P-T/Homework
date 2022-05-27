41e1d5a857a2ff3d5d8f8a76d31e4a4bb6bfac4d
J D <ab@c.ru>
2022-05-27 03:18:40 +0300

    Create remember_result decorator

---
+++ AndreyOzerets/Task5.py
@@ -0,0 +1,28 @@
+# Task 5.5
+
+from functools import wraps
+
+
+def remember_result(func):
+    '''Decorator remembering the last result
+
+    Prints the last result before the next function call
+    '''
+
+    last_result = None
+
+    @ wraps(func)
+    def wrap(*args):
+        nonlocal last_result
+        print(f"Last result = {last_result}")
+
+        if all([isinstance(i, int) or isinstance(i, float) for i in args]):
+            res = 0
+            for i in args:
+                res += i
+            args = str(res)
+            last_result = args
+        else:
+            args = [str(i) for i in args]
+        last_result = func(*args)
+    return wrap
