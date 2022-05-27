620849c97d92b2ed9c1745ac6da0a3c46f9fe47f
J D <ab@c.ru>
2022-05-27 03:07:49 +0300

    Initial condition of task 4

--- AndreyOzerets/Task4.py
+++ AndreyOzerets/Task4.py
@@ -1,1 +1,12 @@
 # Task 5.4
+
+a = "I am global variable!"
+
+
+def enclosing_funcion():
+    a = "I am variable from enclosed function!"
+
+    def inner_function():
+
+        a = "I am local variable!"
+        print(a)
