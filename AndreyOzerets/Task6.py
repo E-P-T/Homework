e514ab761c4face7b37fc7896b5ec2ce259b2332
J D <ab@c.ru>
2022-05-27 03:34:42 +0300

    Create call_once decorator

--- AndreyOzerets/Task6.py
+++ AndreyOzerets/Task6.py
@@ -1,1 +1,17 @@
 # Task 5.6
+
+from functools import wraps
+
+
+def call_once(func):
+    '''Decorator for caching the first result of a function'''
+
+    cached_result = None
+
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        nonlocal cached_result
+        if cached_result is None:
+            cached_result = func(*args, **kwargs)
+        return cached_result
+    return wrap
