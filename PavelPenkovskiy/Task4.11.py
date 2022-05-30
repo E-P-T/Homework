d1611a6d87e4be51c63d46310a568652bbe13f62
pavel <p.penkovskiy@gmail.com>
2022-05-30 20:27:06 +0300

    c_4_c_11

--- PavelPenkovskiy/Task4.11.py
+++ PavelPenkovskiy/Task4.11.py
@@ -5,7 +5,6 @@
 
 # ```python
 # def combine_dicts(*args):
-# ...
 
 # dict_1 = {'a': 100, 'b': 200}
 # dict_2 = {'a': 200, 'c': 300}
@@ -16,3 +15,18 @@
 
 # print(combine_dicts(dict_1, dict_2, dict_3)
 # >>> {'a': 600, 'b': 200, 'c': 300, 'd': 100}
+
+
+def combine_values(*args):
+
+    result = dict()
+
+    for dictionary in args:
+        for k in dictionary.keys():
+            if k not in result.keys():
+                result[k] = 0
+            result[k] = result[k] + dictionary[k]
+    return result
+
+
+print(combine_values({'a': 100, 'b': 200}, {'a': 100, 'b': 200}))
