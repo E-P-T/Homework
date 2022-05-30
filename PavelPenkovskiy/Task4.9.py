ebd16e88ef41efaaaf90fbc79606519a2c7252a3
pavel <p.penkovskiy@gmail.com>
2022-05-30 18:51:41 +0300

    c_4_c_8

--- PavelPenkovskiy/Task4.9.py
+++ PavelPenkovskiy/Task4.9.py
@@ -16,7 +16,57 @@
 # >>> {'a', 'b', 'c', 'f', 'g', 'i', 'j', 'k', 'm', 'q', 's', 'u', 'v', 'x', 'z'}
 
 
+import string
+alphabet = string.ascii_lowercase
 
 
+def test_1_1(*args):
+    result_1 = []
+    for i in alphabet:
+        character_appear_in_all_strings = True
+        for j in list(*args):
+            if i not in j:
+                character_appear_in_all_strings = False
+        if character_appear_in_all_strings:
+            result_1.append(i)
+    return set(result_1)
 
 
+def test_1_2(*args):
+    result_2 = []
+    characters = ''.join(list(*args))
+    for i in characters:
+        if characters.count(i) == 1:
+            result_2.append(i)
+    return set(result_2)
+
+
+def test_1_3(*args):
+    result_3 = []
+    for a in alphabet:
+        count_a = 0
+        for s in list(*args):
+            if a in s:
+                count_a += 1
+        if count_a == 2:
+            result_3.append(a)
+    return set(result_3)
+
+
+def test_1_4(*args):
+    result_4 = []
+    for a in alphabet:
+        count_a = 0
+        for s in list(*args):
+            if a in s:
+                count_a += 1
+        if count_a == 0:
+            result_4.append(a)
+    return str(result_4)
+
+
+strings = ("hello", "world", "python")
+print(test_1_1(strings))
+print(test_1_2(strings))
+print(test_1_3(strings))
+print(test_1_4(strings))
