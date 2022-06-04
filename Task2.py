bbf70a07f21cf539cf509e5e3bd82350151ceb65
J D <ab@c.ru>
2022-06-05 01:24:16 +0300

    create typed_property function

--- Task2.py
+++ Task2.py
@@ -1,1 +1,17 @@
 # Task 6.2
+
+def typed_property(name, expected_type):
+    """Function to check the type of an argument."""
+
+    storage_name = '_' + name
+
+    @property
+    def prop(self):
+        return getattr(self, storage_name)
+
+    @prop.setter
+    def prop(self, value):
+        if not isinstance(value, expected_type):
+            raise TypeError('{} must be a {}'.format(name, expected_type))
+        setattr(self, storage_name, value)
+    return prop
