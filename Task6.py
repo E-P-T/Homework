c921cfac0210866cdbe98e7a55d9b861eb7e6459
J D <ab@c.ru>
2022-06-07 23:28:44 +0300

    create Money clas

--- Task6.py
+++ Task6.py
@@ -1,1 +1,13 @@
 # Task 6.6
+class Money:
+
+    currency_rate = {
+        "USD": 1.0,
+        "EUR": 0.93,
+        "BYN": 2.10,
+        "JPY": 127.94,
+    }
+
+    def __init__(self, amount, currency='USD'):
+        self._amount = amount
+        self._currency = currency
