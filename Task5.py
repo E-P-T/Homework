52858da6d9dee879f145764c3926062f27ba5cc8
J D <ab@c.ru>
2022-06-07 23:18:52 +0300

    craete Sun class

--- Task5.py
+++ Task5.py
@@ -1,1 +1,12 @@
 # Task 6.5
+class Sun:
+    """Singleton class."""
+
+    _instance = None
+
+    @classmethod
+    def inst(cls):
+        print(cls)
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
