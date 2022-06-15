41c54975c2c6ccd685272a2375da0f4605cb40ad
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-06-16 00:27:18 +0400

    Implemented a html converter prototype

--- UladzimirFiodarau/final_task/rss_reader/rss_output.py
+++ UladzimirFiodarau/final_task/rss_reader/rss_output.py
@@ -19,7 +19,7 @@ class RssConverter:
         of RssConverter class is formed
         :param news_dict: a dictionary with news
         :param url: URL of rss-feed
-        :param date: date of news if given
+        :param date: date of news
         """
         self.url = url
         self.date = date
@@ -32,7 +32,7 @@ class RssConverter:
         Method is used to create unique filenames for saving converted news to file. It uses URL, if given, to get its
         network location domain name, and date, if given, to combine unique filename for output.
         :param url: URL of rss-feed
-        :param date: date of news if given
+        :param date: date of news
         :return: a name of the file
         """
         file_date = 'cached_news_' + date.replace(':', '_') if date else f'{datetime.datetime.now():%Y_%m_%d}'
@@ -57,6 +57,14 @@ class PdfConverter(RssConverter):
     If date is specified only cached sources are used without getting information from URL
     """
     def __init__(self, news_dict=None, url: str = '', date: str = ''):
+        """
+        Method is used to take and save a dictionary with news and data of news if given for later processing and
+        creating a file name with file extension for output while object of PdfConverter class is formed
+        :param news_dict: a dictionary with news
+        :param url: URL of rss-feed
+        :param date: date of news
+
+        """
         RssConverter.__init__(self, news_dict, url, date)
         self.file_name = self.file_name + '.pdf'
 
@@ -83,7 +91,7 @@ class PdfConverter(RssConverter):
             obj.cell(tab)
             obj.cell(w=line_length, h=line_height, txt=line, ln=1, align=line_align, link=link)
 
-    def pdf_output(self):
+    def convert_to_pdf(self):
         """
         method converts a dictionary of news into a pdf file saved in 'output' directory which is formed in script
         directory (by default script directory is the 'rss_reader' directory in root directory 'final_task')
@@ -114,14 +122,15 @@ class PdfConverter(RssConverter):
             else:
                 pdf.image(feed_image, 12, 14, 33, 15)
         # printing Feed header
-        PdfConverter.print_cell(pdf, tab=36, text=news['feed_title'], length=53, line_length=164)
+        PdfConverter.print_cell(pdf, tab=36, text=news.get('feed_title', 'No title'), length=53, line_length=164)
         pdf.cell(200, 2, ln=1, align='L')
-        PdfConverter.print_cell(pdf, tab=36, text=news['feed_description'], length=53, line_length=164)
-        pdf.cell(200, 2, ln=1, align='L')
-        PdfConverter.print_cell(pdf, tab=36, text=f"Feed link: {news['feed_link']}", link=news['feed_link'], length=53,
+        PdfConverter.print_cell(pdf, tab=36, text=news.get('feed_description', 'No additional description'), length=53,
                                 line_length=164)
+        pdf.cell(200, 2, ln=1, align='L')
+        feed_link = news.get('feed_link', '')
+        PdfConverter.print_cell(pdf, tab=36, text=feed_link, link=feed_link, length=53, line_length=164)
         pdf.cell(200, 5, txt="=" * 65, ln=1, align='L')
-        # going into a for loop to iterate over gathered/cached news and form a standardised print
+        # 'tdqm' package is used to show user a progress bar while converting news (especially requesting images)
         for num, item in enumerate(tqdm(sorted(news['feed_items'], reverse=True))):
             pdf.set_font('DejaVuMono', 'B', 14)
             news_title = news["feed_items"][item].get("title", "No title provided")
@@ -156,7 +165,7 @@ class PdfConverter(RssConverter):
             if news_desc is None:
                 news_desc = "No description provided"
             PdfConverter.print_cell(pdf, text=news_desc, length=75)
-            # changing font and colour and printing links (news_link formed previously in news_title section)
+            # changing font and colour for printing links (news_link formed previously in news_title section)
             pdf.set_font('DejaVuMono', 'I', 9)
             pdf.cell(200, 4, ln=1, align='L')
             pdf.set_text_color(0, 0, 255)
@@ -175,5 +184,92 @@ class PdfConverter(RssConverter):
         pdf.output(file_name)
 
 
+class HtmlConverter(RssConverter):
+    """
+    A class for converting news data to HTML format
+    """
+    def __init__(self, news_dict=None, url: str = '', date: str = ''):
+        """
+        Method is used to take and save a dictionary with news and data of news if given for later processing and
+        creating a file name with file extension for output while object of PdfConverter class is formed
+        :param news_dict: a dictionary with news
+        :param url: URL of rss-feed
+        :param date: date of news
+        """
+        RssConverter.__init__(self, news_dict, url, date)
+        self.file_name = self.file_name + '.html'
+
+    def convert_to_html(self):
+        """
+        method converts a dictionary of news into a html file saved in 'output' directory which is formed in script
+        directory (by default script directory is the 'rss_reader' directory in root directory 'final_task')
+        It forms HTML document structure in a list and then writes list items to a file.
+        'tdqm' package is used to show user a progress bar
+        :return: None
+        """
+        news = self.news_dict
+        # We will form HTML document structure in a list, starting with head
+        html_buffer = ['<!DOCTYPE html>', '<html>', '<head><title>News gathered by rss_reader</title></head><body>']
+        # forming Feed info block
+        html_buffer.append(f'<h1 style="text-align:left">{"=" * 83}</h1>')
+        feed_image = script_dir + '/rss-header.png'
+        if 'feed_media' in news:
+            if 'type' not in news['feed_media'] or news['feed_media']['type'].startswith('image'):
+                feed_image = news['feed_media']['url']
+        feed_title = news.get('feed_title', 'No title')
+        html_buffer.append(f'<img src="{feed_image}" alt="Logo" width="120" height="60" style="float:left">'
+                           f'<h1 style = "margin-left: 130px">{feed_title}</h1>')
+        feed_desc = news.get('feed_description', 'No additional description')
+        html_buffer.append(f'<h2 style = "margin-left: 130px">{feed_desc}</h2>')
+        feed_link = news.get('feed_link', '')
+        html_buffer.append(f'<h3 style = "margin-left: 130px"><a href="url">{feed_link}</a></h3>')
+        html_buffer.append(f'<h1 style="text-align:left">{"=" * 83}</h1>')
+        # 'tdqm' package is used to show user a progress bar while converting news
+        for item in tqdm(sorted(news['feed_items'], reverse=True)):
+            news_title = news["feed_items"][item].get("title", "No title provided")
+            # checking for media and its type for forming the document
+            news_media = None
+            news_image = None
+            if 'media' in news["feed_items"][item] and 'url' in news["feed_items"][item]['media']:
+                media = news["feed_items"][item]['media']
+                news_media = media['url']  # We will use a link to media later in script
+                if 'type' not in media or media['type'].startswith('image'):
+                    news_image = media['url']  # We will use a link to media later in script
+            tab = 330 if news_image else 0
+            # printing image if found and title
+            if news_image:
+                html_buffer.append(f'<img src="{news_image}" alt="Image" width="320" height="200" style="float:left">'
+                                   f'<h3 style = "margin-left: {tab}px">{news_title}</h3>')
+            else:
+                html_buffer.append(f'<h3 style = "margin-left: 0px">{news_title}</h3>')
+            # printing publication date
+            news_date = f'Publication date: {news["feed_items"][item].get("pubDate", "No publication date provided")}'
+            html_buffer.append(f'<p style = "margin-left: {tab}px">{news_date}</p>')
+            # printing description
+            news_desc = news["feed_items"][item].get("description", "No description provided")
+            html_buffer.append(f'<p style = "margin-left: {tab}px">{news_desc}</p><br>')
+            # printing news link and media link
+            news_link = news["feed_items"][item].get("link", "No link provided")
+            if news_link != "No link provided":
+                html_buffer.append(f'<p style = "margin-left: {tab}px"><a href="url">{news_link}</a><p>')
+            else:
+                html_buffer.append(f'<p style = "margin-left: {tab}px"><{news_link}<p>')
+            if news_media:
+                html_buffer.append(f'<p style = "margin-left: {tab}px"><a href="url">Media link: {news_media}</a><p>')
+            # finishing news item block with printing a separator between news
+            html_buffer.append(f'<p style="text-align:left">{"-" * 280}</p>')
+        # closing previously opened tags
+        html_buffer.append('</body></html>')
+        # checking for output directory and saving html to file
+        RssConverter.check_directory('/output/')
+        file_name = script_dir + '/output/' + self.file_name
+        with open(file_name, 'w', encoding='utf-8') as out:
+            for line in html_buffer:
+                print(line, file=out)
+
+
 if __name__ == '__main__':
-    pass
+    with open(script_dir + '/test_examples/dict_v3.txt', encoding='utf-8') as file:
+        dictionary = eval(file.read())
+        html = HtmlConverter(dictionary)
+        html.convert_to_html()
