6a166a5ff3968ef43bcda0d806fe3f70d6400342
fcumay <voyshnism@mail.ru>
2022-06-17 23:52:33 +0300

    added Task 6.7

--- MayaVoyshnis/Task 6.7.py
+++ MayaVoyshnis/Task 6.7.py
@@ -34,7 +34,7 @@ print(s) #result in “BYN”
 class Money:
     _exchange_rate = {
         'EUR': 0.93,
-        'BYN': 2.1,
+        'BYN': 2.0,
         'USD': 1,
         "JPY": 2.0
     }
@@ -49,29 +49,45 @@ class Money:
 
     def __add__(self, other):
         if isinstance(other, int) or isinstance(other, float):
-            return self.default_currency + other
-        return self.default_currency + other.default_currency
+            return Money(self.default_currency + other)
+        return Money(self.default_currency + other.default_currency)
 
     __radd__ = __add__
 
     def __sub__(self, other):
         if isinstance(other, int) or isinstance(other, float):
-            return self.default_currency - other
-        return self.default_currency - other.default_currency
+            return Money(self.default_currency - other)
+        return Money(self.default_currency - other.default_currency)
 
     __rsub__ = __sub__
+
+    def __mul__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Money(self.default_currency * other)
+        raise ValueError()
+    __rmul__=__mul__
+
+    def __truediv__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Money(self.default_currency / other)
+        raise ValueError()
+    __rtruediv__=__truediv__
+
+    def __str__(self):
+        return f'{self.value} USD'
 
 
 if __name__ == '__main__':
     x = Money(10, "JPY")
     y = Money(11)
-    print(x + y + x + 3)
-    print(7 - x)
-    # x = Money(10, "BYN")
-    # y = Money(11)
-    # z = Money(12.34, "EUR")
-    # print(z + 3.11 * x + y * 0.8)
-    #
-    # lst = [Money(10, "BYN"), Money(11), Money(12.01, "USD")]
-    # s = sum(lst)
-    # print(s)
+    print((x + y + x * 3)*4)
+    print(2*y)
+    x = Money(10, "BYN")
+    y = Money(11)
+    z = Money(12.34, "EUR")
+    print(z + 3.11 * x + y * 0.8)
+
+    lst = [Money(10, "BYN"), Money(11), Money(12.01, "USD")]
+    s = sum(lst)
+    print(s)
+    print(y/11)
