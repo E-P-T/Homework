25d2053f0607e9a1a0c9b49600ddef51012f43ce
pavel <p.penkovskiy@gmail.com>
2022-06-21 17:55:11 +0300

    s_6_c_1

---
+++ PavelPenkovskiy/Task 6.1.py
@@ -0,0 +1,64 @@
+# Task 6.1
+# Implement a Counter class which optionally accepts the start value and the counter stop value.
+# If the start value is not specified the counter should begin with 0.
+# If the stop value is not specified it should be counting up infinitely.
+# If the counter reaches the stop value, print "Maximal value is reached."
+#
+# Implement to methods: "increment" and "get"
+#
+# If you are familiar with Exception rising use it to display the "Maximal value is reached." message.
+#
+# Example:
+# ```python
+# >>> c = Counter(start=42)
+# >>> c.increment()
+# >>> c.get()
+# 43
+#
+# >>> c = Counter()
+# >>> c.increment()
+# >>> c.get()
+# 1
+# >>> c.increment()
+# >>> c.get()
+# 2
+#
+# >>> c = Counter(start=42, stop=43)
+# >>> c.increment()
+# >>> c.get()
+# 43
+# >>> c.increment()
+# Maximal value is reached.
+# >>> c.get()
+# 43
+
+
+class Counter:
+    def __init__(self, start=0, stop=float('inf')):
+        self.start = start
+        self.stop = stop
+
+    def increment(self):
+        if self.start == self.stop:
+            print('Maximal value is reached.')
+        else:
+            self.start += 1
+
+    def get(self):
+        print(self.start)
+
+# c = Counter(start=42)
+# c.increment()
+# c.get()
+
+# c = Counter()
+# c.increment()
+# c.get()
+# c.increment()
+# c.get()
+
+c = Counter(start=42, stop=43)
+c.increment()
+c.get()
+c.increment()
+c.get()
