a0550b36f12eb8c8070441c2126e72889d4a3831
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-06-21 15:52:10 +0400

    a minor change in variable naming and a few updates o dockstring

--- UladzimirFiodarau/final_task/rss_reader/rss_logger.py
+++ UladzimirFiodarau/final_task/rss_reader/rss_logger.py
@@ -4,15 +4,15 @@ This file sets logging configuration, by default, according to the task, logging
 import sys
 import logging
 
-NO_COLOR = "\33[m"
+no_color = "\33[m"
 
 
 def add_color(logger_method, _color):
     """
-    A decorator to enable colouring of logger output methods, uses ANSI escape codes to apply colouring
-    :param logger_method: method to set colour
-    :param _color: 3* for foreground colouring, 4* for background
-    :return: coloured output in wrapped method
+    A decorator to enable coloring of logger output methods, uses ANSI escape codes to apply coloring
+    :param logger_method: method to set color
+    :param _color: 3* for foreground coloring, 4* for background
+    :return: colored output in wrapped method
     """
     def wrapper(message, *args, **kwargs):
         """
@@ -25,7 +25,7 @@ def add_color(logger_method, _color):
         color = kwargs.pop("color", _color)
         if isinstance(color, int):
             color = "\33[%dm" % color
-        return logger_method(color + message+NO_COLOR, *args, **kwargs)
+        return logger_method(color + message + no_color, *args, **kwargs)
     return wrapper
 
 
@@ -39,7 +39,7 @@ logger_info.basicConfig(level=logging.INFO,
                         )
 
 # setting default color of info print to no color and enabling decorator
-setattr(logger_info, "info", add_color(getattr(logger_info, "info"), NO_COLOR))
+setattr(logger_info, "info", add_color(getattr(logger_info, "info"), no_color))
 
 
 if __name__ == '__main__':
