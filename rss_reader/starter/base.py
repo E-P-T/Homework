e3a9642c06d6a35f97f2ce68dc52be647a481b8c
Andrey Ozerets <ab@c.ru>
2022-06-23 11:03:56 +0300

    add docstring

--- rss_reader/starter/base.py
+++ rss_reader/starter/base.py
@@ -8,4 +8,10 @@ version = '0.0.1'
 
 
 def init_arguments_functionality(args=None) -> Dict[str, str]:
-    pass
+    """Create command line options.
+
+    :param args: Command line options, defaults to None.
+    :type args: List, optional.
+    :return: Command line parameter dictionary.
+    :rtype: Dict[str, str]
+    """
