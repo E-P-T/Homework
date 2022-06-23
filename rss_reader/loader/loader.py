13ad94c4b8a0b8877e27fc898e210d68dd113f99
Andrey Ozerets <ab@c.ru>
2022-06-24 00:44:23 +0300

    align indentation in class FromWebHandler

--- rss_reader/loader/loader.py
+++ rss_reader/loader/loader.py
@@ -25,39 +25,39 @@ class FromWebHandler(IHandler):
         self._crawler = crawler
         self._parser = parser
 
-        def get_data(self,
-                     tag_name: str,
-                     title_tag: str,
-                     source: str,
-                     limit: int) -> dict:
-            """Return a dictionary with parsed data.
+    def get_data(self,
+                 tag_name: str,
+                 title_tag: str,
+                 source: str,
+                 limit: int) -> dict:
+        """Return a dictionary with parsed data.
 
-            :param tag_name: The name of the tag in which the news is stored.
-            :type tag_name: str
-            :param title_tag: The name of the tag in which the name
-                                of the rss resource is stored.
-            :type title_tag: str
-            :param source: Resource URL.
-            :type source: str
-            :param limit: Number of news items to display.
-            :type limit: int
-            :return: Dictionary with parsed data.
-            :rtype: dict
-            """
+        :param tag_name: The name of the tag in which the news is stored.
+        :type tag_name: str
+        :param title_tag: The name of the tag in which the name
+                            of the rss resource is stored.
+        :type title_tag: str
+        :param source: Resource URL.
+        :type source: str
+        :param limit: Number of news items to display.
+        :type limit: int
+        :return: Dictionary with parsed data.
+        :rtype: dict
+        """
 
-            cr = self._crawler(source)
-            response_ = cr.get_data()
+        cr = self._crawler(source)
+        response_ = cr.get_data()
 
-            self._parser.create_parser(markup=response_)
-            title_text = next(self._parser.get_tags_text(
-                selector=title_tag))
-            items = self._parser.get_items(
-                self.template, name=tag_name, limit_elms=limit)
+        self._parser.create_parser(markup=response_)
+        title_text = next(self._parser.get_tags_text(
+            selector=title_tag))
+        items = self._parser.get_items(
+            self.template, name=tag_name, limit_elms=limit)
 
-            log.info('Start generating results.')
-            result = {'title_web_resource': title_text}
-            items_dict = {'items': items}
-            result.update(items_dict)
-            log.info('Result was formed.')
+        log.info('Start generating results.')
+        result = {'title_web_resource': title_text}
+        items_dict = {'items': items}
+        result.update(items_dict)
+        log.info('Result was formed.')
 
-            return result
+        return result
