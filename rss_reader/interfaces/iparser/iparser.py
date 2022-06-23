f42588ddf22058b378735e43331e0b98c23394f6
Andrey Ozerets <ab@c.ru>
2022-06-23 22:53:16 +0300

    create abstractmethod get_tags_text

--- rss_reader/interfaces/iparser/iparser.py
+++ rss_reader/interfaces/iparser/iparser.py
@@ -6,3 +6,9 @@ class IParser(ABC):
     @abstractmethod
     def create_parser(self, markup: bytes, features: str = 'xml') -> None:
         pass
+
+    @abstractmethod
+    def get_tags_text(self,
+                      selector: str,
+                      limit_elms: int = None) -> Generator[str, None, None]:
+        pass
