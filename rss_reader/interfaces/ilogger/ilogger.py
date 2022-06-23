05d685dec46b4193550aaa1bb828f92b440a6e65
Andrey Ozerets <ab@c.ru>
2022-06-23 14:39:29 +0300

    add a docstring to the set_config

--- rss_reader/interfaces/ilogger/ilogger.py
+++ rss_reader/interfaces/ilogger/ilogger.py
@@ -6,4 +6,11 @@ from logging import Logger as LG
 class ISetLoggerConfig(ABC):
     @abstractmethod
     def set_config(self, name: str) -> LG:
+        """Set logger configuration.
+
+        :param name: Logger name.
+        :type name: str
+        :return: object Logger.
+        :rtype: LG
+        """
         pass
