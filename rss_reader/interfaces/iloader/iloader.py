c415773b704f4ddd651b1021a22d3a848e0d4b8a
Andrey Ozerets <ab@c.ru>
2022-06-23 18:22:11 +0300

    add a docstring to the get_data

--- rss_reader/interfaces/iloader/iloader.py
+++ rss_reader/interfaces/iloader/iloader.py
@@ -8,4 +8,18 @@ class IHandler(ABC):
                  title_tag: str,
                  source: str,
                  limit: int) -> dict:
+        """Return a dictionary with parsed data.
+
+        :param tag_name: The name of the tag in which the news is stored.
+        :type tag_name: str
+        :param title_tag: The name of the tag in which the name
+                            of the rss resource is stored.
+        :type title_tag: str
+        :param source: Resource URL.
+        :type source: str
+        :param limit: Number of news items to display.
+        :type limit: int
+        :return: Dictionary with parsed data.
+        :rtype: dict
+        """
         pass
