93cae0476d6ab644d5abe6e0f111a678b0c59752
Andrey Ozerets <ab@c.ru>
2022-06-23 14:03:21 +0300

    add a docstring to __init__ of the Logger

--- rss_reader/logger/logger.py
+++ rss_reader/logger/logger.py
@@ -6,5 +6,12 @@ class Logger:
     NAME_LOGGER = 'base_logger'
 
     def __init__(self, name_loger: str, config: ISetLoggerConfig) -> None:
+        """Initializer.
+
+        :param name_loger: Logger name.
+        :type name_loger: str
+        :param config: Logger configuration.
+        :type config: ISetLoggerConfig
+        """
         Logger.NAME_LOGGER = name_loger
         self._config = config
