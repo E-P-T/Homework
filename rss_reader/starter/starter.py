563c872d6ef3e3634cfc2deb3573805df296cfb2
Andrey Ozerets <ab@c.ru>
2022-06-25 01:15:07 +0300

    import JSONViewHandler into the starter module

--- rss_reader/starter/starter.py
+++ rss_reader/starter/starter.py
@@ -9,7 +9,7 @@ from rss_reader.loader.loader import FromWebHandler
 from rss_reader.parser.parser import BeautifulParser
 from rss_reader.crawler.crawler import SuperCrawler
 from rss_reader.interfaces.iviewer.iviewer import IViewHandler
-from rss_reader.viewer.viewer import StandartViewHandler
+from rss_reader.viewer.viewer import StandartViewHandler, JSONViewHandler
 
 from .ecxeptions import NonNumericError
 
