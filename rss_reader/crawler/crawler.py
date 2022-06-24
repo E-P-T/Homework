e80f156379828df728cf1eeeb2760cc06970cbe6
Andrey Ozerets <ab@c.ru>
2022-06-24 09:42:22 +0300

    create the get_data into the SuperCrawler class

--- rss_reader/crawler/crawler.py
+++ rss_reader/crawler/crawler.py
@@ -6,3 +6,6 @@ from rss_reader.interfaces.icrawler.icrawler import ICrawler
 class SuperCrawler(ICrawler):
     def __init__(self, url: str) -> None:
         self._url = url
+
+    def get_data(self) -> bytes:
+        pass
