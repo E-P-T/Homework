5c7067990158d6026bdb32eb9a0d6b6c5e71ba62
Andrey Ozerets <ab@c.ru>
2022-06-24 12:30:49 +0300

    add a dockstring to the find_all method

--- rss_reader/interfaces/iparser/isubsystem.py
+++ rss_reader/interfaces/iparser/isubsystem.py
@@ -6,6 +6,16 @@ from typing import Iterable
 class ISubsystem(ABC):
 
     def find_all(self, name: str = None, limit: int = None) -> Iterable:
+        """Return all tags with the given name.
+
+        :param name: A filter on tag name, defaults to None
+        :type name: str, optional
+        :param limit: Stop looking after finding this many results,
+                      defaults to None.
+        :type limit: int, optional
+        :return: An iterable object that provides the found elements.
+        :rtype: Iterable
+        """
         pass
 
     def select(self, selector: str, limit: int = None) -> Iterable:
