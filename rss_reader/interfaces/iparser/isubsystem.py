86ff1bdca4b904b66a8865031d39b8d837da9a07
Andrey Ozerets <ab@c.ru>
2022-06-24 12:25:52 +0300

    create the select method in the ISubsystem

--- rss_reader/interfaces/iparser/isubsystem.py
+++ rss_reader/interfaces/iparser/isubsystem.py
@@ -7,3 +7,6 @@ class ISubsystem(ABC):
 
     def find_all(self, name: str = None, limit: int = None) -> Iterable:
         pass
+
+    def select(self, selector: str, limit: int = None) -> Iterable:
+        pass
