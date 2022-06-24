d5fee51a03a4378e7abfaef635782e07c9a14f2b
Andrey Ozerets <ab@c.ru>
2022-06-24 11:08:22 +0300

    add a docstring to the FailStatusCodeError

--- rss_reader/crawler/exceptions.py
+++ rss_reader/crawler/exceptions.py
@@ -1,6 +1,8 @@
 
 
 class BadURLError(Exception):
+    """"Occurs when the url is specified incorrectly."""
+
     def __init__(self, url, *args, **kwargs) -> None:
         self.url = url
         super().__init__(*args, **kwargs)
