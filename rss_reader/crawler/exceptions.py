f76717b0e73767de04e2c57c85b97b3092471787
Andrey Ozerets <ab@c.ru>
2022-06-24 10:19:59 +0300

    override __str__ method in BadURLError

--- rss_reader/crawler/exceptions.py
+++ rss_reader/crawler/exceptions.py
@@ -4,3 +4,6 @@ class BadURLError(Exception):
     def __init__(self, url, *args, **kwargs) -> None:
         self.url = url
         super().__init__(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f'It is not possible to get data for the given url ({self.url})'
