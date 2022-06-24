425bcb7ff25eeeece86caa123f9d34e32d41f7b1
Andrey Ozerets <ab@c.ru>
2022-06-24 08:14:46 +0300

    get program logger

--- rss_reader/decorator/decorator.py
+++ rss_reader/decorator/decorator.py
@@ -2,6 +2,8 @@
 from functools import wraps
 from rss_reader.logger.logger import Logger
 
+log = Logger.get_logger(__name__)
+
 
 def send_log_of_start_function(func):
     def wrapper(*args, **kwargs):
