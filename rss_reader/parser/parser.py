6d6bda95e90a24786c8df01c89c7750a70fccfde
Andrey Ozerets <ab@c.ru>
2022-06-24 12:42:28 +0300

    add a docstrint to the __init__ in the BeautifulParser

--- rss_reader/parser/parser.py
+++ rss_reader/parser/parser.py
@@ -6,4 +6,9 @@ from rss_reader.interfaces.iparser.isubsystem import ISubsystem
 class BeautifulParser(IParser):
 
     def __init__(self, subsystem: ISubsystem) -> None:
+        """Initializer.
+
+        :param subsystem: Third party parser.
+        :type subsystem: ISubsystem
+        """
         self._subsystem = subsystem
