1a76cecc017cfcff081d1da3a7a16d8aecbd4b39
Andrey Ozerets <ab@c.ru>
2022-06-24 23:39:20 +0300

    add @send_log_of_start_function to the set_next

--- rss_reader/viewer/viewer.py
+++ rss_reader/viewer/viewer.py
@@ -7,6 +7,7 @@ from rss_reader.interfaces.iviewer.iviewer import IViewHandler
 class AbstractViewHandler(IViewHandler):
     _next_handler: Optional[IViewHandler] = None
 
+    @send_log_of_start_function
     def set_next(self, handler: IViewHandler) -> IViewHandler:
         self._next_handler = handler
         return handler
