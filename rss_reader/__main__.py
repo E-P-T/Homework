76ceba7d81b0f5e607b73e071e6e7213a1d36bcb
Andrey Ozerets <ab@c.ru>
2022-06-25 13:49:13 +0300

    import the EmptyListError into the __main__ module

--- rss_reader/__main__.py
+++ rss_reader/__main__.py
@@ -5,6 +5,7 @@ from rss_reader.starter.base import (create_logger,
                                      init_arguments_functionality as iaf)
 from rss_reader.logger.logger import Logger
 from rss_reader.starter.ecxeptions import NonNumericError
+from rss_reader.parser.exceptions import EmptyListError
 
 
 def main():
