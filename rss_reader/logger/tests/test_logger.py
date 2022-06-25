8d9e1280b120d90643fa47ec2d3a75c1be1741ba
Andrey Ozerets <ab@c.ru>
2022-06-25 23:04:53 +0300

    add a dockstring to the test_stream_config

--- rss_reader/logger/tests/test_logger.py
+++ rss_reader/logger/tests/test_logger.py
@@ -4,6 +4,7 @@ from ..logger import StreamHandlerConfig
 
 
 def test_stream_config():
+    """Check that the type of logger returned is of type Logger."""
     sc = StreamHandlerConfig()
     logger = sc.set_config('name')
     assert isinstance(logger, Logger)
