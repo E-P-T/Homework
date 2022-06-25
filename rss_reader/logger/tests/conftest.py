afdc259b1f27bc2af0a224cc2adadb33d99b0e4b
Andrey Ozerets <ab@c.ru>
2022-06-25 23:29:59 +0300

    make the logger_obj method a fixture

--- rss_reader/logger/tests/conftest.py
+++ rss_reader/logger/tests/conftest.py
@@ -3,5 +3,8 @@ import pytest
 from ..logger import Logger
 
 
+@pytest.fixture(scope="class",
+                params=[StreamHandlerConfig,
+                        NullHandlerConfig])
 def logger_obj(request):
     yield Logger('test_name', request.param())
