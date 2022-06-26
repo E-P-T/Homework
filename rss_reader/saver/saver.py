f110d3ffdd32a23bf39936322be6e6f65810dc01
Andrey Ozerets <ab@c.ru>
2022-06-26 23:10:18 +0300

    override set_next in the AbstractSaveHandler

--- rss_reader/saver/saver.py
+++ rss_reader/saver/saver.py
@@ -1,5 +1,10 @@
 
 
 class AbstractSaveHandler(ISaveHandler):
-    
+
     _next_handler: Optional[ISaveHandler] = None
+
+    @send_log_of_start_function
+    def set_next(self, handler: ISaveHandler) -> ISaveHandler:
+        self._next_handler = handler
+        return handler
