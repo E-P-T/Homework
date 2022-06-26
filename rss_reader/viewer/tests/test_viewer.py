78008d644802751332f9f9202cf5b24eb5251548
Andrey Ozerets <ab@c.ru>
2022-06-26 09:11:44 +0300

    create test_StandartViewHandler_show test

--- rss_reader/viewer/tests/test_viewer.py
+++ rss_reader/viewer/tests/test_viewer.py
@@ -3,3 +3,13 @@
 from json import dumps
 
 from ..viewer import JSONViewHandler, StandartViewHandler
+
+
+def test_StandartViewHandler_show(capsys):
+
+    data = {'title_web_resource': 'mock_show'}
+    json_obj = StandartViewHandler()
+    json_obj.show(data)
+    out, err = capsys.readouterr()
+    out = out.strip('\n')
+    assert out == 'Feed: : mock_show'
