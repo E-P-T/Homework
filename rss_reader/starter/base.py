3bcd2772359ab27ee14eac30c0fb28192c56bf08
Andrey Ozerets <ab@c.ru>
2022-06-26 12:32:18 +0300

    del default value from the argparse

--- rss_reader/starter/base.py
+++ rss_reader/starter/base.py
@@ -32,8 +32,6 @@ def init_arguments_functionality(args=None) -> Dict[str, str]:
     )
 
     parser.add_argument('source',
-                        nargs='?',
-                        default='https://news.yahoo.com/rss/',
                         help='RSS URL')
 
     parser.add_argument('--version',
