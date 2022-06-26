f986b6a74e2822675756d7aca03b5044d79299e9
Andrey Ozerets <ab@c.ru>
2022-06-26 10:38:16 +0300

    create mock_crawler fixture

--- rss_reader/tests/loader/loader.py
+++ rss_reader/tests/loader/loader.py
@@ -5,3 +5,11 @@ import pytest
 from rss_reader.loader.loader import FromWebHandler
 from rss_reader.crawler.crawler import SuperCrawler
 from rss_reader.parser.parser import BeautifulParser
+
+
+@pytest.fixture
+def mock_crawler(mocker):
+    m = __name__ + '.SuperCrawler'
+    mock_cls = mocker.patch(m)
+    mock_cp = mock_cls.return_value
+    mock_cp.get_data.return_value = "Response!"
