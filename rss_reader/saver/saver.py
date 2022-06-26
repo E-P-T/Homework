e5fdd0e8a6047dc6f7042ff8933eeaa41a50e39d
Andrey Ozerets <ab@c.ru>
2022-06-26 23:10:41 +0300

    override save in the AbstractSaveHandler

--- rss_reader/saver/saver.py
+++ rss_reader/saver/saver.py
@@ -8,3 +8,8 @@ class AbstractSaveHandler(ISaveHandler):
     def set_next(self, handler: ISaveHandler) -> ISaveHandler:
         self._next_handler = handler
         return handler
+
+    @send_log_of_start_function
+    def save(self, data: dict, file: str):
+        if self._next_handler:
+            return self._next_handler.show(data)
