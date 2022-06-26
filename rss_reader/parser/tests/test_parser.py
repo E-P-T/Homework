adb45e9c28b1816512e157e17cfc6a9b9cc56fb8
Andrey Ozerets <ab@c.ru>
2022-06-26 06:51:01 +0300

    create test_get_tags_text_type test

--- rss_reader/parser/tests/test_parser.py
+++ rss_reader/parser/tests/test_parser.py
@@ -18,3 +18,13 @@ class MockTags:
     @property
     def text(self):
         return ['mock_1_text', 'mock_2_text']
+
+
+def test_get_tags_text_type(monkeypatch):
+
+    def mock_tag(*args, **kwargs):
+        return MockTags
+
+    monkeypatch.setattr(BeautifulParser, "_select", mock_tag)
+    title_ = BeautifulParser(object).get_tags_text('title')
+    assert isinstance(title_, Generator)
