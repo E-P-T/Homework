02de44db78a8c8fc04e22d735a8bb9bd149a1291
Andrey Ozerets <ab@c.ru>
2022-06-26 08:35:53 +0300

    add a dockstring to the test_init_return_json

--- rss_reader/starter/tests/test_base.py
+++ rss_reader/starter/tests/test_base.py
@@ -14,5 +14,6 @@ def test_init_arguments_functionality(option):
 
 @pytest.mark.parametrize("option", [("https://y.com", "--json",)])
 def test_init_return_json(option):
+    """Check if a key exists in a dictionary."""
     a = iaf(option)
     assert 'json' in a
