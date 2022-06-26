14dcac95484674b1a8e402ca64a783ac1a917aaf
Andrey Ozerets <ab@c.ru>
2022-06-26 08:32:08 +0300

    create the test_init_arguments_functionality function

--- rss_reader/starter/tests/test_base.py
+++ rss_reader/starter/tests/test_base.py
@@ -3,3 +3,9 @@
 import pytest
 
 from ..base import init_arguments_functionality as iaf
+
+
+@pytest.mark.parametrize("option", [("https://y.com",)])
+def test_init_arguments_functionality(option):
+    a = iaf(option)
+    assert isinstance(a, dict)
