8c5f9dda4c1725946e848a8653aea75d6f6d338a
Andrey Ozerets <ab@c.ru>
2022-06-26 23:01:56 +0300

    add a dockstring to the set_next method

--- rss_reader/interfaces/isaver/isaver.py
+++ rss_reader/interfaces/isaver/isaver.py
@@ -5,6 +5,14 @@ from abc import ABC, abstractmethod
 class ISaveHandler(ABC):
     @abstractmethod
     def set_next(self, handler: ISaveHandler) -> ISaveHandler:
+        """Set the next saver in the handler chain.
+
+        :param handler: Next handler.
+        :type handler: ISaveHandler
+        :return: Handler.
+        :rtype: ISaveHandler
+        """
+
         pass
 
     def save(self, data: dict):
