a8a1a3769fc8c1832f6bb2aba2c2274f50cc56cb
Andrey Ozerets <ab@c.ru>
2022-06-26 06:53:46 +0300

    create the test_get_tags_text_EmptyListError test

--- rss_reader/parser/tests/test_parser.py
+++ rss_reader/parser/tests/test_parser.py
@@ -32,3 +32,14 @@ def test_get_tags_text_type(monkeypatch):
     monkeypatch.setattr(BeautifulParser, "_select", mock_tag)
     title_ = BeautifulParser(object).get_tags_text('title')
     assert isinstance(title_, Generator)
+
+
+def test_get_tags_text_EmptyListError(monkeypatch):
+
+    def mock_raise(*args, **kwargs):
+        return []
+
+    monkeypatch.setattr(BeautifulParser, "_select", mock_raise)
+
+    with pytest.raises(EmptyListError):
+        next(BeautifulParser(object).get_tags_text('title'))
