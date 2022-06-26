ded74231dd8ca4437e97c85d2785cb2e5c15323c
Andrey Ozerets <ab@c.ru>
2022-06-26 10:39:45 +0300

    create the mock_parser fixture

--- rss_reader/tests/loader/loader.py
+++ rss_reader/tests/loader/loader.py
@@ -15,3 +15,14 @@ def mock_crawler(mocker):
     mock_cls = mocker.patch(m)
     mock_cp = mock_cls.return_value
     mock_cp.get_data.return_value = "Response!"
+
+
+@pytest.fixture
+def mock_parser(mocker):
+
+    m = __name__ + '.BeautifulParser'
+    mock_cls = mocker.patch(m)
+    mock_cp = mock_cls.return_value
+    mock_cp.create_parser.return_value = "Create!"
+    mock_cp.get_tags_text.return_value = iter(["Title!"])
+    mock_cp.get_items.return_value = [{'mock_item': 1}]
