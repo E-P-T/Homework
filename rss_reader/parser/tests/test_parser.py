45988cb1a9386e255f88dfbe6dfbc31c71a7b2fd
Andrey Ozerets <ab@c.ru>
2022-06-26 06:47:18 +0300

    create MockTags class into the test_parser

--- rss_reader/parser/tests/test_parser.py
+++ rss_reader/parser/tests/test_parser.py
@@ -6,3 +6,10 @@ from collections.abc import Generator
 
 from ..parser import BeautifulParser
 from ..exceptions import EmptyListError
+
+
+class MockTags:
+
+    @property
+    def text(self):
+        return ['mock_1_text', 'mock_2_text']
