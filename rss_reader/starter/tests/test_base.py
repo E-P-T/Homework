5c84a42dc33e86caa273879b903cf9d81a2e0ad1
Andrey Ozerets <ab@c.ru>
2022-06-26 08:33:39 +0300

    add a dockstring to the test_init_arguments_functionality

--- rss_reader/starter/tests/test_base.py
+++ rss_reader/starter/tests/test_base.py
@@ -7,5 +7,6 @@ from ..base import init_arguments_functionality as iaf
 
 @pytest.mark.parametrize("option", [("https://y.com",)])
 def test_init_arguments_functionality(option):
+    """Check if dictionary returned"""
     a = iaf(option)
     assert isinstance(a, dict)
