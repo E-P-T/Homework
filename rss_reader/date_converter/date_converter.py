98982baf1accf4591849950cb25953c901741c6a
Andrey Ozerets <ab@c.ru>
2022-06-27 23:25:02 +0300

    return the date as a string

--- rss_reader/date_converter/date_converter.py
+++ rss_reader/date_converter/date_converter.py
@@ -5,4 +5,4 @@ from rss_reader.interfaces.idateconverter.idateconverter import IDateConverter
 class DateConverter(IDateConverter):
     @staticmethod
     def date_convert(date: str, format: str = '%Y%m%d') -> str:
-        pass
+        return datetime.strptime(date, format).date().__str__()
