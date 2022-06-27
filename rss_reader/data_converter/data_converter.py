cdd0f30cc02d2ee9054092b534132abc0791cc75
Andrey Ozerets <ab@c.ru>
2022-06-27 19:54:47 +0300

    add a dockstring to the concat_data method

--- rss_reader/data_converter/data_converter.py
+++ rss_reader/data_converter/data_converter.py
@@ -9,6 +9,8 @@ from rss_reader.interfaces.idataconverter.idataconverter import IDataConverter
 class DataConverter(IDataConverter):
 
     def concat_data(self, data, local_data) -> Optional[DataFrame]:
+        """Concat two DataFrames."""
+
         norm_data = json_normalize(data, record_path=['items'],
                                    meta=['title_web_resource', 'link'],
                                    record_prefix="item.")
