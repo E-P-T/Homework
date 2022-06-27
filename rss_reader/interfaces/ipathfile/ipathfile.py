21de7e2863e2cfabdb419e16fb7eb4a1da4cdb48
Andrey Ozerets <ab@c.ru>
2022-06-27 20:37:35 +0300

    add a docstring to the create_file abstract method

--- rss_reader/interfaces/ipathfile/ipathfile.py
+++ rss_reader/interfaces/ipathfile/ipathfile.py
@@ -6,4 +6,9 @@ class ICreateFile(ABC):
 
     @abstractmethod
     def create_file(self, file: str) -> None:
+        """Create file.
+
+        :param file: File name.
+        :type file: str
+        """
         pass
