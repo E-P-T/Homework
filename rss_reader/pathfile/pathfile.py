84b65d820120f49b73cd6f34c404652b4c5eda55
Andrey Ozerets <ab@c.ru>
2022-06-27 20:55:41 +0300

    add a dockstring to the create_file method

--- rss_reader/pathfile/pathfile.py
+++ rss_reader/pathfile/pathfile.py
@@ -6,5 +6,10 @@ from rss_reader.interfaces.ipathfile.ipathfile import ICreateFile
 
 class PathFile(ICreateFile):
     def create_file(self, file: str) -> None:
+        """Create file.
+
+        :param file: File name.
+        :type file: str
+        """
         file = Path(file)
         file.touch(exist_ok=True)
