59f7bb4aaa123952d469f04a8e4fdb4cd531b376
Andrey Ozerets <ab@c.ru>
2022-06-27 19:34:28 +0300

    normalize the received data

--- rss_reader/data_converter/data_converter.py
+++ rss_reader/data_converter/data_converter.py
@@ -9,4 +9,6 @@ from rss_reader.interfaces.idataconverter.idataconverter import IDataConverter
 class DataConverter(IDataConverter):
 
     def concat_data(self, data, local_data) -> Optional[DataFrame]:
-        pass
+        norm_data = json_normalize(data, record_path=['items'],
+                                   meta=['title_web_resource', 'link'],
+                                   record_prefix="item.")
