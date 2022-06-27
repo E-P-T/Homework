0ff81267a0acb2ec0b77b9103d0f4db16b693884
0iskak <isnu260102@gmail.com>
2022-06-27 21:23:03 +0600

    added unit tests

---
+++ test/test_Logger.py
@@ -0,0 +1,30 @@
+import io
+from contextlib import redirect_stdout
+from unittest import TestCase
+from src.Logger import Logger, Level
+
+
+class TestLogger(TestCase):
+    def test_log(self):
+        Logger.verbose(True)
+        string = 'test'
+
+        with io.StringIO() as buf1:
+            with redirect_stdout(buf1):
+                Logger.log(Level.INFO, string)
+                Logger.log(Level.WARNING, string)
+                self.assertRaises(SystemExit, Logger.log, Level.ERROR, string)
+            for line in buf1.getvalue().strip().split('\n'):
+                self.assertRegex(line, string)
+
+        Logger.verbose(False)
+        with io.StringIO() as buf2:
+            with redirect_stdout(buf2):
+                Logger.log(Level.INFO, string)
+                Logger.log(Level.WARNING, string)
+            self.assertEqual(len(buf2.getvalue()), 0)
+
+        with io.StringIO() as buf3:
+            with redirect_stdout(buf3):
+                self.assertRaises(SystemExit, Logger.log, Level.ERROR, string)
+            self.assertRegex(buf3.getvalue(), string)
---
+++ test/test_News.py
@@ -0,0 +1,32 @@
+import datetime
+from unittest import TestCase
+from src.news.News import News
+
+
+class TestNews(TestCase):
+    def test_parse_json(self):
+        feed = 'test_feed'
+        title = 'test_title'
+        date = datetime.datetime.today()
+        link = 'test_link'
+        images = ['test_image']
+        obj = {
+            'feed': feed,
+            'items': [{
+                'title': title,
+                'date': str(date),
+                'link': link,
+                'images': images
+            }]
+        }
+
+        news = News.parse_json(obj)
+        self.assertEqual(news.feed, feed)
+        self.assertEqual(len(news.items), 1)
+
+        item = news.items[0]
+        self.assertEqual(item.title, title)
+        self.assertEqual(item.date, date)
+        self.assertEqual(item.link, link)
+        self.assertEqual(item.images, images)
+
