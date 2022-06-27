a52726294dd8f73a1ff772263bb8477fc24a0938
Andrey Ozerets <ab@c.ru>
2022-06-27 19:42:29 +0300

    import to_datetime into the data_converter

--- rss_reader/data_converter/data_converter.py
+++ rss_reader/data_converter/data_converter.py
@@ -1,7 +1,7 @@
 
 
 from typing import Optional
-from pandas import DataFrame, json_normalize, concat
+from pandas import DataFrame, json_normalize, concat, to_datetime
 
 from rss_reader.interfaces.idataconverter.idataconverter import IDataConverter
 
