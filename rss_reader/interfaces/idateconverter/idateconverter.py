525158540d46cc12793474a24ed9f5260474fdea
Andrey Ozerets <ab@c.ru>
2022-06-27 23:09:54 +0300

    add a docstring to the date_convert method

--- rss_reader/interfaces/idateconverter/idateconverter.py
+++ rss_reader/interfaces/idateconverter/idateconverter.py
@@ -3,4 +3,14 @@ class IDateConverter(ABC):
     @staticmethod
     @abstractmethod
     def date_convert(date: str, format: str) -> str:
+        """Return the textual representation of the substring.
+
+        :param date: Date in original state.
+        :type date: str
+        :param format: Substring selection format.
+                        Example '%Y%m%d'.
+        :type format: str
+        :return: Date in the given format.
+        :rtype: str
+        """
         pass
