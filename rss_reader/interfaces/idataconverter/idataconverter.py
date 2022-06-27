8bf8fea40bdc2182b1024ad34b3ea7e69f898b05
Andrey Ozerets <ab@c.ru>
2022-06-27 07:19:54 +0300

    change docstring in the idataconverter module

--- rss_reader/interfaces/idataconverter/idataconverter.py
+++ rss_reader/interfaces/idataconverter/idataconverter.py
@@ -10,6 +10,16 @@ class IDataConverter(ABC):
     """Basic converter interface."""
 
     @abstractmethod
-    def concat_data(self, data: List[dict], local_data: str) -> Optional[DataFrame]:
-        """Get normalized data."""
+    def concat_data(self, data: List[dict],
+                    local_data: str) -> Optional[DataFrame]:
+        """Get normalized data.
+
+        :param data: Data for concatenation.
+        :type data: List[dict]
+        :param local_data: The name of the file where the locally saved data
+                            is stored.
+        :type local_data: str
+        :return: DataFrame with merged data. Duplicates are removed.
+        :rtype: Optional[DataFrame]
+        """
         pass
