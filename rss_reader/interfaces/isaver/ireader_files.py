b8b6caaf5084fe36460ee81ce95b93cd667c9a25
Andrey Ozerets <ab@c.ru>
2022-06-27 20:14:47 +0300

    add a dockstring to the read_csv_file in the IReadFile

--- rss_reader/interfaces/isaver/ireader_files.py
+++ rss_reader/interfaces/isaver/ireader_files.py
@@ -6,4 +6,20 @@ class IReadFile(ABC):
                       index_col_: str,
                       creater: ICreateFile,
                       encoding_: str = 'utf-8') -> Optional[DataFrame]:
+        """Read csv file.
+
+        :param file: File name.
+        :type file: str
+        :param index_col_: Column(s) to use as the row labels of the DataFrame,
+                        either given as string name or column index. If a
+                        sequence of int / str is given, a MultiIndex is used.
+        :type index_col_: str
+        :param creater: An object that implements the ability to create a file.
+                        Used when the requested file does not exist.
+        :type creater: ICreateFile
+        :param encoding_: File encoding, defaults to 'utf-8'
+        :type encoding_: str, optional
+        :return: Return the read data as a DataFrame.
+        :rtype: Optional[DataFrame]
+        """
         pass
