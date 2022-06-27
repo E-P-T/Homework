3fffc80d7f6e83b0918480cb5c3ec270c5085a02
Andrey Ozerets <ab@c.ru>
2022-06-27 19:37:30 +0300

    import json_normalize in the data_converter module

--- rss_reader/data_converter/data_converter.py
+++ rss_reader/data_converter/data_converter.py
@@ -1,7 +1,7 @@
 
 
 from typing import Optional
-from pandas import DataFrame
+from pandas import DataFrame, json_normalize
 
 from rss_reader.interfaces.idataconverter.idataconverter import IDataConverter
 
