519781707731f17386d9fe1086ae1b963fb5807b
Andrey Ozerets <ab@c.ru>
2022-06-27 21:47:44 +0300

    read csv file

--- rss_reader/saver/reader_files.py
+++ rss_reader/saver/reader_files.py
@@ -9,3 +9,6 @@ class ReaderFiles(IReadFile):
                       creater: ICreateFile,
                       encoding_: str = 'utf-8') -> Optional[DataFrame]:
         local_storage = None
+        local_storage = read_csv(file,
+                                 index_col=index_col_,
+                                 encoding=encoding_)
