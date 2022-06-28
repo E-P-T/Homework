628185ef3a698a4fd6f05b68c3e6584234eb5b63
Andrey Ozerets <ab@c.ru>
2022-06-29 00:06:05 +0300

    create a loader object

--- rss_reader/starter/starter.py
+++ rss_reader/starter/starter.py
@@ -4,13 +4,16 @@ from typing import Dict
 from bs4 import BeautifulSoup
 
 from rss_reader.logger.logger import Logger
-from rss_reader.interfaces.iloader.iloader import IHandler
-from rss_reader.loader.loader import FromWebHandler
+from rss_reader.interfaces.iloader.iloader import IHandler, ILoadHandler
+from rss_reader.loader.loader import FromWebHandler, FromLocalSTorageHandler
 from rss_reader.parser.parser import BeautifulParser
 from rss_reader.crawler.crawler import SuperCrawler
 from rss_reader.crawler.exceptions import BadURLError
 from rss_reader.interfaces.iviewer.iviewer import IViewHandler
 from rss_reader.viewer.viewer import StandartViewHandler, JSONViewHandler
+
+from rss_reader.interfaces.isaver.isaver import ISaveHandler
+from rss_reader.saver.saver import LocalSaveHandler
 
 from .ecxeptions import NonNumericError
 
@@ -31,30 +34,12 @@ class Starter:
     def run(self) -> None:
         """Program launch."""
 
-        if self._argv['source']:
-            log.info("Get the number of requested news.")
-
-            try:
-                lim = self._argv.get('limit')
-                limit = int(lim) if lim else None
-            except ValueError as e:
-                log.error(e)
-                raise NonNumericError("--limit has a non-numeric value") from e
-
-            log.info("Number was received.")
-
-            data_handler = self._get_data_from_resource()
-            try:
-                data = data_handler.get_data('item',
-                                             'channel > title',
-                                             self._argv.get('source'),
-                                             limit)
-            except BadURLError as e:
-                log.error(e)
-                raise
-
-            if not data['items']:
-                data['items'] = [{'no news': 'Sorry, no news'}]
+        data_handler = self._get_data_from_resource()
+        try:
+            data = data_handler.get_data()
+        except BadURLError as e:
+            log.error(e)
+            raise
 
         log.info("Start getting the viewer object.")
         viewer = self._get_viewer(self._argv)
@@ -64,11 +49,39 @@ class Starter:
         viewer.show(data)
         log.info("Stop displaying data.")
 
-    def _get_data_from_resource(self) -> IHandler:
-        """Get data handler."""
-        web_hendler = FromWebHandler(SuperCrawler,
-                                     BeautifulParser(BeautifulSoup))
-        return web_hendler
+        self._get_saver().save(data)
+
+    def _get_limit(self) -> None:
+        log.info("Get the number of requested news.")
+        try:
+            lim = self._argv.get('limit')
+            limit = int(lim) if lim else None
+        except ValueError as e:
+            log.exception(e)
+            raise NonNumericError("--limit has a non-numeric value") from e
+
+        if limit is not None and limit < 0:
+            raise ValueError('--limit must be positive')
+        log.info("Number was received.")
+
+        self._argv['limit'] = limit
+
+    def _get_data_from_resource(self) -> ILoadHandler:
+        """настроить обработчик иданных"""
+
+        self._get_limit()
+
+        wh = FromWebHandler('item',
+                            'channel > title',
+                            'channel > link',
+                            self._argv.get('source'),
+                            self._argv.get('limit'),
+                            SuperCrawler,
+                            BeautifulParser(BeautifulSoup))
+
+        ls = FromLocalSTorageHandler('local_storage.csv', self._argv)
+        ls.set_next(wh)
+        return ls
 
     def _get_viewer(self, request: Dict[str, str]) -> IViewHandler:
         """Get data viewer."""
@@ -76,3 +89,8 @@ class Starter:
         json_ = JSONViewHandler(request)
         json_.set_next(stdout_)
         return json_
+
+    def _get_saver(self) -> ISaveHandler:
+        standart_saver = LocalSaveHandler()
+
+        return standart_saver
