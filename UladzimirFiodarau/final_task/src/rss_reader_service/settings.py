756034cd4e9785a42aee77051a6bcc8ba986717e
Uladzimir Fiodarau <ufiodarau@gmail.com>
2022-06-29 15:28:27 +0400

    Changed settings.py to get DEBUG and SECRET_KEY settings from environment settings of docker-compose.yml

--- UladzimirFiodarau/final_task/src/rss_reader_service/settings.py
+++ UladzimirFiodarau/final_task/src/rss_reader_service/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uisa8_14sgu-7cm2h0d2(-sg)7psu7!wdc(2p1^gx)x85joi02'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
