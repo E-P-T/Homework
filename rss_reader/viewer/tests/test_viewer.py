ac580f2b363d5b82a41ff22f18eb16631438c8bb
Andrey Ozerets <ab@c.ru>
2022-06-30 00:52:26 +0300

    put the dictionary in the list

--- rss_reader/viewer/tests/test_viewer.py
+++ rss_reader/viewer/tests/test_viewer.py
@@ -8,7 +8,7 @@ from ..viewer import JSONViewHandler, StandartViewHandler
 def test_StandartViewHandler_show(capsys):
     """check that the show method prints the correct data to stdout."""
 
-    data = {'title_web_resource': 'mock_show'}
+    data = [{'title_web_resource': 'mock_show'}]
     json_obj = StandartViewHandler()
     json_obj.show(data)
     out, err = capsys.readouterr()
