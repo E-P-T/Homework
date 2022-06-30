8923721c266dcfb60002fda6465f1a3a70b30f3b
Andrey Ozerets <ab@c.ru>
2022-07-01 00:32:57 +0300

    get file to save

--- rss_reader/saver/to_pdf/to_pdf.py
+++ rss_reader/saver/to_pdf/to_pdf.py
@@ -8,6 +8,6 @@ class PDFSaveHandler(AbstractSaveHandler):
     def __init__(self, request: Dict[str, str], folder: str) -> None:
         self._request = request
         self._folder = folder
-    
+
     def save(self, data: List[dict]) -> None:
-        pass
+        file = self._request.get('to_pdf')
