3dbc0851cf8b67b023c36a88ecb6c806c75fe733
Andrey Ozerets <ab@c.ru>
2022-06-30 19:24:46 +0300

    add a dockstring to the prepare_html in the SuperStrategySaveHTML

--- rss_reader/saver/to_html/strategies.py
+++ rss_reader/saver/to_html/strategies.py
@@ -9,7 +9,19 @@ from rss_reader.interfaces.isaver.istrategies import StrategySaveHTML
 
 class SuperStrategySaveHTML(StrategySaveHTML):
     """Implements the simplest strategy for generating an HTML file."""
+
     def prepare_html(self, data: List[dict]) -> str:
+        """Prepare html page.
+
+        The operation of preparing a page according to a specific algorithm.
+        Each specific strategy defines its own page creation logic.
+
+        :param data: Initial data intended for display on the html page.
+        :type data: List[dict]
+        :return: Generated html page according to a certain algorithm.
+        :rtype: str
+        """
+
         path_ = pathlib.Path(__file__).parent
         file_loader = FileSystemLoader(path_/'templates')
         env = Environment(loader=file_loader)
