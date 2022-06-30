bd35969d59f68c01270d3fa8b609de2f28995af6
Andrey Ozerets <ab@c.ru>
2022-06-30 19:10:43 +0300

    add a docjstring to the prepare_html in the StrategySaveHTML

--- rss_reader/interfaces/isaver/istrategies.py
+++ rss_reader/interfaces/isaver/istrategies.py
@@ -8,4 +8,14 @@ class StrategySaveHTML(ABC):
     """Strategy interface."""
     @abstractmethod
     def prepare_html(self, data: List[dict]) -> str:
+        """Prepare html page.
+
+        The operation of preparing a page according to a specific algorithm.
+        Each specific strategy defines its own page creation logic.
+
+        :param data: Initial data intended for display on the html page.
+        :type data: List[dict]
+        :return: Generated html page according to a certain algorithm.
+        :rtype: str
+        """
         pass
