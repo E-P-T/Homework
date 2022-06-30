f4edff17efaf799b77977334d623f16b9aae6fa4
Andrey Ozerets <ab@c.ru>
2022-06-30 19:12:41 +0300

    import StrategySaveHTML into the strategies module

---
+++ rss_reader/saver/to_html/strategies.py
@@ -0,0 +1,13 @@
+
+from rss_reader.interfaces.isaver.istrategies import StrategySaveHTML
+
+
+class SuperStrategySaveHTML(StrategySaveHTML):
+    def prepare_html(self, data: List[dict]) -> str:
+        path_ = pathlib.Path(__file__).parent
+        file_loader = FileSystemLoader(path_/'templates')
+        env = Environment(loader=file_loader)
+        template_ = env.get_template('main.html')
+        html_ = template_.render(data=data)
+
+        return html_
