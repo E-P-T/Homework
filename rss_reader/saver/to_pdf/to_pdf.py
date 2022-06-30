0dac30e559ebe38ea11cb97efa9cf57586a0959e
Andrey Ozerets <ab@c.ru>
2022-07-01 00:31:33 +0300

    override __init__ method

--- rss_reader/saver/to_pdf/to_pdf.py
+++ rss_reader/saver/to_pdf/to_pdf.py
@@ -5,5 +5,9 @@ from ..saver import AbstractSaveHandler
 
 
 class PDFSaveHandler(AbstractSaveHandler):
+    def __init__(self, request: Dict[str, str], folder: str) -> None:
+        self._request = request
+        self._folder = folder
+    
     def save(self, data: List[dict]) -> None:
         pass
