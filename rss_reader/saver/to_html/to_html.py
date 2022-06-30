c6c46c47fc15de643a888665812fcd084db427c6
Andrey Ozerets <ab@c.ru>
2022-06-30 07:52:01 +0300

    override __init__ method in the HTMLSaveHandler class

--- rss_reader/saver/to_html/to_html.py
+++ rss_reader/saver/to_html/to_html.py
@@ -6,5 +6,14 @@ from ..saver import AbstractSaveHandler
 
 
 class HTMLSaveHandler(AbstractSaveHandler):
+    def __init__(self, request: Dict[str, str]) -> None:
+        """Initializer.
+
+        :param request: A dictionary in which there may be a key
+                        by which this handler will work.
+        :type request: Dict[str, str]
+        """
+        self._request = request
+
     def save(self, data: List[dict], file: str) -> None:
         pass
