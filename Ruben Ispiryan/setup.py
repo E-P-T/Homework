66ed5740939a77fffd62ad4d922bb8a4dbed6c08
ruben.ispir <rubeni@redkite.io>
2022-07-01 03:11:12 +0400

    added library requirements

--- Ruben Ispiryan/setup.py
+++ Ruben Ispiryan/setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='rss_reader',
-    version='1.0',
+    version='1.3',
     url='https://github.com/rubenispiryan/Homework/blob/final_task/',
     author='Ruben Ispiryan',
     packages=['rss_reader_pckg', 'rss_reader_pckg.rss', 'rss_reader_pckg.tests'],
@@ -10,6 +10,10 @@ setup(
         'requests',
         'beautifulsoup4',
         'lxml',
+        'python-dateutil',
+        'xhtml2pdf',
+        'airium',
+
     ],
     entry_points={'console_scripts': ['rss_reader=rss_reader_pckg.rss_reader:main']},
     classifiers=[
