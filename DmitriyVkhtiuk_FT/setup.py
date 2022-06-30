cd0115390a44101c6ab0cea2491e2f61605092d9
Dmitriy Vikhtiuk <vihtodim@gmail.com>
2022-06-30 19:38:05 +0300

    2 iteration is done. Script runs in two ways. As a CLI utility and as a script. Checked on clean machine

---
+++ DmitriyVkhtiuk_FT/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+package_data = \
+    {'': ['*']}
+
+with open("requirements.txt", "r", encoding="utf-8") as reqs:
+    requirements = reqs.read()
+entry_points = \
+    {'console_scripts': ['rss_reader = rss_reader.rss_reader:main']}
+
+setup_kwargs = {
+    'name': 'rss-reader',
+    'version': 1.2,
+    'description': 'A simple CLI rss reader',
+    'author': 'DVikhtiuk',
+    'author_email': 'dimastol1ca@gmail.com',
+    'packages': find_packages(),
+    'package_data': package_data,
+    'install_requires': requirements,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+setup(**setup_kwargs)
