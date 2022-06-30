ff034f35ab422b37f4a74a83a363035b48335069
Andrey Ozerets <ab@c.ru>
2022-06-30 18:54:12 +0300

    create StrategySaveHTML class

--- rss_reader/interfaces/isaver/istrategies.py
+++ rss_reader/interfaces/isaver/istrategies.py
@@ -1,1 +1,7 @@
 """This module contains a set of interfaces for HTML strategies."""
+
+
+class StrategySaveHTML(ABC):
+    @abstractmethod
+    def prepare_html(self, data: List[dict]) -> str:
+        pass
