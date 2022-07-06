a5d1aea9d26b83416b971fc42e48760a41418611
J D <ab@c.ru>
2022-07-06 11:27:21 +0300

    override __enter__

--- Task3.py
+++ Task3.py
@@ -6,3 +6,8 @@ class ExecutionTimeToLogFile(ContextDecorator):
     def __init__(self, file) -> None:
         super().__init__()
         self._file = file
+
+    def __enter__(self):
+        print('Starting')
+        self._start = time()
+        return self
