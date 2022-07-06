ae04f6f5fd188bae5cfdef4a0838b5db4bff5082
J D <ab@c.ru>
2022-07-06 12:45:53 +0300

    create WrongTypeError

--- AndreyOzerets/Task5.py
+++ AndreyOzerets/Task5.py
@@ -11,3 +11,10 @@ class MyBaseError(Exception):
 
     def __str__(self):
         return f'{self._number} - {self._message}'
+
+
+class WrongTypeError(MyBaseError):
+    """Class for type errors"""
+
+    def __init__(self, number, message='is not a number') -> None:
+        super().__init__(number, message)
