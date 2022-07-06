fcd15a6af56f237170049e7449b725d249c45032
J D <ab@c.ru>
2022-07-06 22:45:57 +0300

    del Iterable from stop variable

--- AndreyOzerets/Task7.py
+++ AndreyOzerets/Task7.py
@@ -27,7 +27,7 @@ def typed_prop(name, expectrd_type):
 
 class MyNumberCollection:
     start = typed_prop('start', (int, Iterable))
-    stop = typed_prop('stop', (int, Iterable, type(None)))
+    stop = typed_prop('stop', (int, type(None)))
     step = typed_prop('step', (int, type(None)))
 
     def __init__(self, start, stop=None, step=None) -> None:
