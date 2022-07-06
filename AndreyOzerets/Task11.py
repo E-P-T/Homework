de78d78caed0767003f1941f389edae58eab7235
J D <ab@c.ru>
2022-07-06 23:44:53 +0300

    create endless_fib_generator function

--- AndreyOzerets/Task11.py
+++ AndreyOzerets/Task11.py
@@ -1,1 +1,14 @@
 # Task 7.11
+
+
+def endless_fib_generator() -> Generator[int, None, None]:
+    """Infinite fibonacci numbers.
+
+    :yield: Fibonacci number at each iteration.
+    :rtype: Generator[int, None, None]
+    """
+
+    first, second = 0, 1
+    while True:
+        yield second
+        first, second = second, first + second
