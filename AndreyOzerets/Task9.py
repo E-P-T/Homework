a0a2ed5f2efcf4b8e5bbf39bdf9c2a288f530ea8
J D <ab@c.ru>
2022-07-06 23:27:41 +0300

    cerate typed_prop function

--- AndreyOzerets/Task9.py
+++ AndreyOzerets/Task9.py
@@ -1,1 +1,18 @@
 # Task 7.9
+
+def typed_prop(name, expectrd_type):
+    """Type checking"""
+
+    storage_name = '_' + name
+
+    @property
+    def prop(self):
+        return getattr(self, storage_name)
+
+    @prop.setter
+    def prop(self, value):
+        if not isinstance(value, expectrd_type):
+            raise TypeError(f'{name} must be a {expectrd_type}')
+        setattr(self, storage_name, value)
+
+    return prop
