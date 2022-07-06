5132409d540326d629136d17d8b3a9f919ea9f52
J D <ab@c.ru>
2022-07-06 09:29:22 +0300

    call the main program

--- Task2.py
+++ Task2.py
@@ -9,3 +9,18 @@ def my_open(file, mode='r'):
         yield f
     finally:
         f.close()
+
+
+if __name__ == '__main__':
+
+    try:
+        with my_open('a.txt', 'w') as mo:
+            mo.write('OK')
+    except FileNotFoundError as e:
+        _print_ex()
+        print(f'\n*** Is the filename correct?: {e}', end='\n\n')
+        raise SystemExit
+    except ValueError as e:
+        _print_ex()
+        print(f'\n*** Is the mode correct?: {e}', end='\n\n')
+        raise SystemExit
