ef32b6459815e31ed7310cefe2bbdb807bc5eb2f
J D <ab@c.ru>
2022-07-06 09:01:21 +0300

    override __enter__

--- AndreyOzerets/Task1.py
+++ AndreyOzerets/Task1.py
@@ -1,7 +1,12 @@
 # Task 7.1
 
+
 class MyOpen:
     def __init__(self, file, mode='r') -> None:
         self._file = file
         self._mode = mode
         self._file_obj = None
+
+    def __enter__(self):
+        self._file_obj = open(self._file, self._mode)
+        return self._file_obj
