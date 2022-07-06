3b4ca0cbb7e2cf3e042781a8ce05009e6024681e
J D <ab@c.ru>
2022-07-06 09:27:47 +0300

    create my_open function

--- Task2.py
+++ Task2.py
@@ -1,1 +1,11 @@
 # Task 7.2
+from contextlib import contextmanager
+
+
+@contextmanager
+def my_open(file, mode='r'):
+    f = open(file, mode)
+    try:
+        yield f
+    finally:
+        f.close()
