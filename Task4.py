35e946c15e225701543bd9d09aa351448da2ac7d
J D <ab@c.ru>
2022-07-06 11:49:09 +0300

    override __enter__

--- Task4.py
+++ Task4.py
@@ -5,3 +5,8 @@ class SupressDecorator(ContextDecorator):
     def __init__(self, file) -> None:
         super().__init__()
         self._file = file
+
+    def __enter__(self):
+        print('Starting')
+        self._start = time()
+        return self
